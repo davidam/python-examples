7e7032e37937b4fe440b3055e077ffb4521f88a7
David Arroyo Menéndez <davidam@es.gnu.org>
2017-03-16 12:08:14 +0100

    creation

---
+++ print.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+
+print(100 + 35)
+print(300 / 4.6)
+print(300 // 4.6)
+print(200 % 3)
+
+print("sobre" + "impreso")
+print("por favor" * 3)
+
+print("tu" and "yo")
+print("yo" and "tu")
+print("yo" and None and "el")
+
+print(20 > 15)
+print("cielo" == "tierra")
+
+print(5 & 3)
+print(5<<1)
+print(5<<2)
+print(9>>1)
+
+guess = 5
+print("The number is " + str(guess) + " Congratulations!")
