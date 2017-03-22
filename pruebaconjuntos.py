10d2ee6fc0cd2fe5503a7ef9b790899f2420b388
David Arroyo Menéndez <davidam@es.gnu.org>
2017-03-22 12:21:01 +0100

    creation

---
+++ pruebaconjuntos.py
@@ -0,0 +1,27 @@
+
+conjunto = "Italia", "Grecia", "Italia", "China", "Grecia", "Brasil"
+paises = set(conjunto)
+europa = set({"Francia", "Italia", "España", "Grecia", "Alemania", "Portugal"})
+
+paises.remove("China")
+paises.add("Turquia")
+paises.add("Alemania")
+print("contenido actual del conjunto de paises:")
+for pais in paises:
+    print(pais.ljust(15), end="")
+
+print("\n\ncontenido actual del conjunto de Europa:")
+for pais in europa:
+    print(pais.ljust(15), end="")
+
+print("\n\nUnion de los dos conjuntos:")
+for pais in paises | europa:
+    print(pais.ljust(15), end="")
+
+print("\n\nIntersección de los dos conjuntos (elementos en común):")
+for pais in paises.intersection(europa):
+    print(pais.ljust(15), end="")
+
+print("\n\ndiferencia simétrica de los dos conjuntos (elementos que no están en ambos conjuntos):")
+for pais in paises.symmetric_difference(europa):
+    print(pais.ljust(15), end="")
