e1e44f4d4643b34464ee9d7b8b6939be20e782dd
David Arroyo Menéndez <davidam@es.gnu.org>
2017-03-22 08:55:58 +0100

    creation

--- lists.py
+++ lists.py
@@ -8,6 +8,17 @@ print(mylist[0]) # prints 1
 print(mylist[1]) # prints 2
 print(mylist[2]) # prints 3
 
+li = ["a", "b", "mpilgrim", "z", "example"]
+li[0]  
+li.extend(["two", "elements"])
+li.insert(2, "new")
+li.index("example")
+li.remove("example")
+
+lista = ['a', 'b', 'mpilgrim']
+lista = lista + ['example', 'new']
+lista += ['two']
+
 # prints out 1,2,3
-for x in mylist:
+for x in li:
     print(x)
