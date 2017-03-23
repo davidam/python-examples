6c3e39b0f7563497485c9db3b57b0702d569cc08
David Arroyo Menéndez <davidam@es.gnu.org>
2017-03-23 13:35:22 +0100

    creation

---
+++ botonesgtk.py
@@ -0,0 +1,14 @@
+from gi.repository import Gtk
+
+def boton_pulsado(elboton):
+    print("El boton pone " + elboton.get_label())
+    print("Has hecho click")
+
+ventana = Gtk.Window(title="Ventana principal")
+ventana.connect("delete-event", Gtk.main_quit)
+ventana.set_title("Nuevo titulo")
+boton = Gtk.Button(label="Haz clic")
+boton.connect("clicked", boton_pulsado)
+ventana.add(boton)
+ventana.show_all()
+Gtk.main()
