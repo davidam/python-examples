d7e49588c4c56dbff11b4bbb320a97b05e3c9cf3
David Arroyo Menéndez <davidam@es.gnu.org>
2017-03-25 20:35:32 +0100

    verpython.py: fixed

--- verpython.py
+++ verpython.py
@@ -1,6 +1,8 @@
-ver = input("¿Quieres ver mi python? (Si | No): ")
+# encoding: utf-8
+
+ver = raw_input("¿Quieres ver mi python? (Si | No): ")
 if (ver == "Si"):
     print("Te quiero")
-elif ver == "No":
+elif (ver == "No"):
     print("Eres una estrecha")
 
