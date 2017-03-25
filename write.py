c7f9d3e2be91896f05f18ab6957363029a109fb9
David Arroyo Menéndez <davidam@es.gnu.org>
2017-03-25 15:03:22 +0100

    write.py: fix comments

--- write.py
+++ write.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 
-# Abrimos el archivo codehero.txt
+# Abrimos el archivo fichero.txt
 fo = open("/tmp/fichero.txt", "wb")
 fo.write( "Gora python\n");
 
-# Cerramos el archivo codehero.txt
+# Cerramos el archivo fichero.txt
 fo.close()
