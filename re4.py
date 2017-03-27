0b54708a2db4fb5e01bb79ca9e04451289c07e11
David Arroyo Menéndez <davidam@es.gnu.org>
2017-03-27 22:28:03 +0200

    re4.py: fixed

--- re4.py
+++ re4.py
@@ -1,5 +1,7 @@
- str = 'purple alice@google.com, blah monkey bob@abc.com blah dishwasher'
+import re
+
+str = 'purple alice@google.com, blah monkey bob@abc.com blah dishwasher'
   ## re.sub(pat, replacement, str) -- returns new string with all replacements,
   ## \1 is group(1), \2 group(2) in the replacement
-  print re.sub(r'([\w\.-]+)@([\w\.-]+)', r'\1@yo-yo-dyne.com', str)
+print re.sub(r'([\w\.-]+)@([\w\.-]+)', r'\1@yo-yo-dyne.com', str)
   ## purple alice@yo-yo-dyne.com, blah monkey bob@yo-yo-dyne.com blah dishwasher
