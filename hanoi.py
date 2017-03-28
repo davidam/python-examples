00703fbf5a259de2a3d3c2cf338ce181bd5dae71
David Arroyo Menéndez <davidam@es.gnu.org>
2017-03-28 20:57:05 +0200

    hanoi.py: creation

---
+++ hanoi.py
@@ -0,0 +1,10 @@
+def hanoi(discos):
+    discos = input("Dime tus discos y te digo los pasos: ")
+    print(hanoiAux(int(discos)))
+
+def hanoiAux(discos):
+    if (discos == 1):
+        return 1
+    else:
+        return 1 + (2 * (hanoiAux(discos - 1)))
+
