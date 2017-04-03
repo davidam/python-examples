6c8dab3603bff80d0ba3ff026d0a7020f91b52f0
David Arroyo Menéndez <davidam@es.gnu.org>
2017-04-03 21:58:18 +0200

    calculator: add methods

--- calculator/app/calculator.py
+++ calculator/app/calculator.py
@@ -2,5 +2,11 @@
 class Calculator(object):
     def add(self, x, y):
         return x+y
+    def sub(self, x, y):
+        return x-y
+    def prod(self, x, y):
+        return x*y
+    def div(self, x, y):
+        return x/y
 
     
--- calculator/test/test_calculator.py
+++ calculator/test/test_calculator.py
@@ -7,7 +7,23 @@ class TddInPythonExample(unittest.TestCase):
         calc = Calculator()
         result = calc.add(2,2)
         self.assertEqual(4, result)
- 
- 
+
+    def test_calculator_sub_method_returns_correct_result(self):
+        calc = Calculator()
+        result = calc.sub(2,2)
+        self.assertEqual(0, result)
+
+        
+    def test_calculator_prod_method_returns_correct_result(self):
+        calc = Calculator()
+        result = calc.prod(2,2)
+        self.assertEqual(4, result)
+        
+    def test_calculator_div_method_returns_correct_result(self):
+        calc = Calculator()
+        result = calc.div(2,2)
+        self.assertEqual(1, result)
+
+        
 if __name__ == '__main__':
     unittest.main()
