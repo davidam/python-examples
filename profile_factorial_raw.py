81780bcd228880c14828b58ec746f518ac693e18
David Arroyo Menéndez <davidam@es.gnu.org>
2017-04-19 17:40:14 +0200

    profile_factorial_raw.py: creation

---
+++ profile_factorial_raw.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+import profile
+
+def factorial(n):
+    if ((n == 0) | (n == 1)):
+        return 1
+    elif (n > 1):
+        return n * factorial(n - 1)
+
+print "RAW"
+profile.run('factorial(4)')
+    
