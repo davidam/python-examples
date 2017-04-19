2ef9bda42f2a270ad551434f57dee257681d155a
David Arroyo Menéndez <davidam@es.gnu.org>
2017-04-19 16:45:12 +0200

    shape: creation

---
+++ shape/app/shape.py
@@ -0,0 +1,25 @@
+#An example of a class
+class Shape:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.description = "This shape has not been described yet"
+        self.author = "Nobody has claimed to make this shape yet"
+
+    def area(self):
+        return self.x * self.y
+
+    def perimeter(self):
+        return 2 * self.x + 2 * self.y
+
+    def describe(self, text):
+        self.description = text
+
+    def authorName(self, text):
+        self.author = text
+
+    def scaleSize(self, scale):
+        self.x = self.x * scale
+        self.y = self.y * scale
+
