e446a67c16e977d0d1c311f40457a84cd801ff3b
David Arroyo Menéndez <davidam@es.gnu.org>
2017-04-19 15:52:37 +0200

    factorial: creation

---
+++ factorial/app/factorial.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+class Factorial(object):
+    def fac(self, n):
+        if ((n == 0) | (n == 1)):
+            return 1
+        elif (n > 1):
+            return n * self.fac(n - 1)
+
+# x = input("Choose a number: ")
+# f = Factorial()
+
+# print f.fac(x)     
