470d50cf3e0a3fffd2a9c0266e71bdbb8d13946b
David Arroyo Menéndez <davidam@es.gnu.org>
2017-05-02 20:52:20 +0200

    fixed

--- poo/factorial/app/factorial.py
+++ poo/factorial/app/factorial.py
@@ -5,9 +5,9 @@ class Factorial(object):
         if ((n == 0) | (n == 1)):
             return 1
         elif (n > 1):
-            return n * fac(n - 1)
+            return n * self.fac(n - 1)
 
-x = input("Choose a number: ")
-f = Factorial()
+# x = input("Choose a number: ")
+# f = Factorial()
 
-print f.fac(x)     
+# print f.fac(x)     
