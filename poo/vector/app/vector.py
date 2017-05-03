00e514701bcdc788879be562e94a60ca2c0c586c
David Arroyo Menéndez <davidam@es.gnu.org>
2017-05-03 20:05:16 +0200

    creation

---
+++ poo/vector/app/vector.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+class Vector:
+   def __init__(self, a, b):
+      self.a = a
+      self.b = b
+
+   def __str__(self):
+      return 'Vector (%d, %d)' % (self.a, self.b)
+   
+   def __add__(self,other):
+      return Vector(self.a + other.a, self.b + other.b)
+
