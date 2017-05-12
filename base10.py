e6248d23e4200da90805a5a85a30f1d82bb63568
David Arroyo Menéndez <davidam@es.gnu.org>
2017-05-12 20:16:44 +0200

    base10.py: fix input

--- base10.py
+++ base10.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- 
 # Obtenemos datos.
-nbinario = raw_input('Número binario (4 cifras): ')
+nbinario = raw_input('Número binario: ')
 
 # Obtenemos los dígitos.
 nbinario = nbinario.split(',')
