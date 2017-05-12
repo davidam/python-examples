1f4e879db906489f23069915af0c6153d56a6af3
David Arroyo Menéndez <davidam@es.gnu.org>
2017-05-12 16:13:18 +0200

    creation

---
+++ statistics-example.py
@@ -0,0 +1,18 @@
+import statistics
+
+example_list = [5,2,5,6,1,2,6,7,2,6,3,5,5]
+
+x = statistics.mean(example_list)
+print(x)
+
+y = statistics.median(example_list)
+print(y)
+
+z = statistics.mode(example_list)
+print(z)
+
+a = statistics.stdev(example_list)
+print(a)
+
+b = statistics.variance(example_list)
+print(b)
