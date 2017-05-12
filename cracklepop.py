b9a597075b81e32ff683ef3a9b190a961b7697e0
David Arroyo Menéndez <davidam@es.gnu.org>
2017-05-12 16:49:21 +0200

    cracklepop.py: creation

---
+++ cracklepop.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+for i in range(1, 101):
+    if ((i % 3) == 0) and ((i % 5) == 0): 
+        print "cracklepop"
+    if (i % 3) == 0: 
+        print "crackle"
+    elif (i % 5) == 0:
+        print "pop"
+    elif ((i % 3) != 0) | ((i % 5) != 0):
+        print i
