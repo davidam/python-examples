42bdc9765caaa08a23238a46f967b6f4010f1563
David Arroyo Menéndez <davidam@es.gnu.org>
2017-05-13 12:28:14 +0200

    numpy-datatypes.py: creation

---
+++ numpy-datatypes.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+import numpy as np
+
+x = np.array([1, 2])  # Let numpy choose the datatype
+print x.dtype         # Prints "int64"
+
+x = np.array([1.0, 2.0])  # Let numpy choose the datatype
+print x.dtype             # Prints "float64"
+
+x = np.array([1, 2], dtype=np.int64)  # Force a particular datatype
+print x.dtype 
