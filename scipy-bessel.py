b375e239d34eb08d9adfaef84a0179be31f2e237
David Arroyo Menéndez <davidam@es.gnu.org>
2017-05-15 20:08:48 +0200

    scipy-bessel.py: creation

---
+++ scipy-bessel.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import division
+
+#----------------------------------------------------------------------
+# Nombre:       ejemplo_scipy_bessel.py
+# PropÃ³sito:    Aprender el uso el paquete special de scipy.
+#
+# Origen:        Propio.
+# Autor:         JosÃ© MarÃ­a Herrera-Fernandez, Luis Miguel SÃ¡nchez-Brea
+#
+# CreaciÃ³n:        18 de Septiembre de 2013
+# Historia:
+#
+# Dependencias:    scipy, matplotlib
+# Licencia:        GPL
+#----------------------------------------------------------------------
+
+
+"""
+DescripciÃ³n: CÃ¡lculo de los coeficientes de orden cero y uno de la funciÃ³n de Bessel 
+contenida en el paquete special.
+"""
+from scipy import special                   # Importamos scipy.special
+import scipy as sp                          # Importamos scipy como el alias sp
+import matplotlib.pyplot as plt             # Importamos matplotlib.pyplot como el alias plt.
+
+# Creamos el array dimensional
+x = sp.arange(0,50,.1)
+print x
+# Calculamos los coeficientes de orden cero.
+j0 = special.j0(x)
+print j0
+# Calculamos los coeficientes de orden uno.
+j1 = special.j1(x)
+print j1
