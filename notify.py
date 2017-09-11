739cf40af9451692d73c7d4558a290d214594f27
David Arroyo Menendez <davidam@gmail.com>
2017-09-11 16:35:04 +0200

    creation

---
+++ notify.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Listener:
+     def __init__(self, name, subject):
+         self.name = name
+         subject.register(self)
+
+     def notify(self, event):
+         print self.name, "received event", event
+
+
+class Subject:
+     def __init__(self):
+         self.listeners = []
+ 
+     def register(self, listener):
+         self.listeners.append(listener)
+ 
+     def unregister(self, listener):
+         self.listeners.remove(listener)
+ 
+     def notify_listeners(self, event):
+         for listener in self.listeners:
+             listener.notify(event)
+
+
+subject = Subject()
+listenerA = Listener("<listener A>", subject)
+listenerA = Listener("<listener AA>", subject)
+listenerB = Listener("<listener B>", subject)
+# El objeto Subject ahora tiene dos "escuchadores" registrados
+subject.notify_listeners("<events>")
