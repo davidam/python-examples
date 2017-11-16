8f10b8dffb5dddd2c0b9c5056d70cf99c66faccb
David Arroyo Menendez <davidam@gmail.com>
2017-11-16 19:39:07 +0100

    pruebaconjuntos.py: fix print and add coding

--- pruebaconjuntos.py
+++ pruebaconjuntos.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 conjunto = "Italia", "Grecia", "Italia", "China", "Grecia", "Brasil"
 paises = set(conjunto)
@@ -8,20 +10,20 @@ paises.add("Turquia")
 paises.add("Alemania")
 print("contenido actual del conjunto de paises:")
 for pais in paises:
-    print(pais.ljust(15), end="")
+    print(pais.ljust(15))
 
 print("\n\ncontenido actual del conjunto de Europa:")
 for pais in europa:
-    print(pais.ljust(15), end="")
+    print(pais.ljust(15))
 
 print("\n\nUnion de los dos conjuntos:")
 for pais in paises | europa:
-    print(pais.ljust(15), end="")
+    print(pais.ljust(15))
 
 print("\n\nIntersección de los dos conjuntos (elementos en común):")
 for pais in paises.intersection(europa):
-    print(pais.ljust(15), end="")
+    print(pais.ljust(15))
 
 print("\n\ndiferencia simétrica de los dos conjuntos (elementos que no están en ambos conjuntos):")
 for pais in paises.symmetric_difference(europa):
-    print(pais.ljust(15), end="")
+    print(pais.ljust(15))
