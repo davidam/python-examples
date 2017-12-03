7b8daeafebe9b269b8ecaedb0ae6e66d9727b5e7
David Arroyo Menendez <davidam@gmail.com>
2017-12-03 02:53:12 +0100

    findall.py: creation

---
+++ regex/findall.py
@@ -0,0 +1,13 @@
+# Import regex package
+import re
+
+# Define sentence
+sentence = 'peter piper pick a peck of pickled peppers'
+
+# Define regex
+ps = 'p\w+'
+
+
+# Find all words in sentence that match the regex and print them
+print re.findall(ps, sentence)
+
