07328d83d6832683a840cb89bdd99eac3d3c0fe6
David Arroyo Menendez <davidam@gmail.com>
2017-12-09 00:18:47 +0100

    scipy-delaunay-triangulations.py: creation

---
+++ scipy/scipy-delaunay-triangulations.py
@@ -0,0 +1,16 @@
+from scipy.spatial import Delaunay
+import numpy as np
+points = np.array([[0, 0], [0, 1.1], [1, 0], [1, 1]])
+tri = Delaunay(points)
+
+import matplotlib.pyplot as plt
+plt.triplot(points[:,0], points[:,1], tri.simplices.copy())
+plt.plot(points[:,0], points[:,1], 'o')
+
+for j, p in enumerate(points):
+    plt.text(p[0]-0.03, p[1]+0.03, j, ha='right') # label the points
+for j, s in enumerate(tri.simplices):
+    p = points[s].mean(axis=0)
+    plt.text(p[0], p[1], '#%d' % j, ha='center') # label triangles
+plt.xlim(-0.5, 1.5); plt.ylim(-0.5, 1.5)
+plt.show()
