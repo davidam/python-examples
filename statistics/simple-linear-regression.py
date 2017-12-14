fe3b4d9eb686c1c4a254ccee99d2a696f92576b4
David Arroyo Menendez <davidam@gmail.com>
2017-12-14 13:58:17 +0100

    simple-linear-regression.py: move to statistics

---
+++ statistics/simple-linear-regression.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas
+x = np.linspace(-5, 5, 20)
+np.random.seed(1)
+# normal distributed noise
+y = -5 + 3*x + 4 * np.random.normal(size=x.shape)
+# Create a data frame containing all the relevant variables
+data = pandas.DataFrame({'x': x, 'y': y})
+
+from statsmodels.formula.api import ols
+model = ols("y ~ x", data).fit()
+
+print(model.summary())  
