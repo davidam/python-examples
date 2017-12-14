573b3e9c0be2aecd27fba8c4070682dd9758ef1b
David Arroyo Menendez <davidam@gmail.com>
2017-12-14 18:13:56 +0100

    code_dictionary.py: small fix

--- nltk/code_dictionary.py
+++ nltk/code_dictionary.py
@@ -1,17 +1,16 @@
 # Natural Language Toolkit: code_dictionary
-# http://www.nltk.org/book/ch05.html
 
 from collections import defaultdict
 counts = defaultdict(int)
-
 from nltk.corpus import brown
 for (word, tag) in brown.tagged_words(categories='news', tagset='universal'):
-    counts[tag] += 1
+     counts[tag] += 1
+
 print counts['NOUN']
 print sorted(counts)
 
 from operator import itemgetter
-print sorted(counts.items(), key=itemgetter(1), reverse=True)
-print [t for t, c in sorted(counts.items(), key=itemgetter(1), reverse=True)]
+sorted(counts.items(), key=itemgetter(1), reverse=True)
+[t for t, c in sorted(counts.items(), key=itemgetter(1), reverse=True)]
 
 
