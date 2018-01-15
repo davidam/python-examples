d2d5f834d13e1d074a69c211f36422250028aef2
David Arroyo Menendez <davidam@gmail.com>
2018-01-15 17:46:19 +0100

    semaphore.py: creation

---
+++ threads/semaphore.py
@@ -0,0 +1,28 @@
+import threading
+import time
+sem = threading.Semaphore()
+
+def fun1():
+    while True:
+        try: 
+            sem.acquire()
+            print(1)
+            sem.release()
+            time.sleep(0.25)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+
+def fun2():
+    while True:
+        try:
+            sem.acquire()
+            print(2)
+            sem.release()
+            time.sleep(0.25)
+        except (KeyboardInterrupt, SystemExit):
+            raise
+
+t = threading.Thread(target = fun1)
+t.start()
+t2 = threading.Thread(target = fun2)
+t2.start()
