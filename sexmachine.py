a0c9a105ce316576cc65e6dfc13804ce8fe8e296
David Arroyo Menendez <davidam@gmail.com>
2018-01-16 13:01:54 +0100

    sexmachine.py: remove file

--- sexmachine.py
+++
@@ -1,8 +0,0 @@
-import sexmachine.detector as gender
-d = gender.Detector()
-if (d.get_gender(u"Bob") == 'male'):
-    print("Python")
-elif (d.get_gender(u"Sally") == 'female'):
-    print("Shell")
-else:
-    print("PHP")
