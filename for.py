e0241686b0ca92ac40650afaf5d90e87f3a256c4
David Arroyo Menendez <davidam@gmail.com>
2018-01-19 15:56:05 +0100

    for.py: add more for methods

--- for.py
+++ for.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python
 
 words = ['cat', 'window', 'defenestrate']
+
+print("######################## First for: #######################")
 for w in words:
     print w, len(w)
+
+print("######################## Second for: #####################")
+    
+for i,w in enumerate(words):
+    print i, w
+
+print("######################## Third for: #####################")
+    
+for i in range(1,10):
+    print i
