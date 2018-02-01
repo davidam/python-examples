9e95af572901590141ba30fb53765900b7f98aae
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-02-01 19:48:51 +0100

    hexadecimal.py: fix regex

--- regex/hexadecimal.py
+++ regex/hexadecimal.py
@@ -2,7 +2,8 @@ import os, re
 
 num = raw_input("Write a hexadecimal number: ")
 
-print("Find any hexadecimal number in a larger body of text: ")
+
+print("Do you find any hexadecimal number in a larger body of text?: ")
 match = re.search(r'\b[0-9A-F]+\b', num)
 # If-statement after search() tests if it succeeded
 if match:                      
@@ -11,7 +12,7 @@ else:
     print("Not, it isn't")
 
 print("Check wheter a text string holds just a hexadecimal number: ")
-match = re.search(r'\b[0-9A-F]+\b', num)
+match = re.search(r'\b[0-9A-F]{6}\b', num)
 # If-statement after search() tests if it succeeded
 if match:                      
     print("Yes, it's") # match.group() ## 'found word:cat'
