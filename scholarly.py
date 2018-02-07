9d3b3495a24f6eeb471f2319048a564c7440b2b0
David Arroyo Menendez <davidam@gmail.com>
2018-02-07 23:00:42 +0100

    scholarly.py: creation

---
+++ scholarly.py
@@ -0,0 +1,13 @@
+import scholarly
+
+print(next(scholarly.search_author('Steven A. Cholewiak')))
+
+# search_query = scholarly.search_author('Manish Singh, Rutgers')
+# print(next(search_query))
+
+# search_query = scholarly.search_keyword('Haptics')
+# print(next(search_query))
+
+# search_query = scholarly.search_pubs_query('Perception of physical stability and center of mass of 3D objects')
+# print(next(search_query))
+
