87913b56419bdcaadc588e93934beb550c511e7c
David Arroyo Menendez <davidam@gmail.com>
2018-02-07 15:36:47 +0100

    dsa.py, whois.py: creation

---
+++ security/whois.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+# Boston, MA 02110-1301 USA,
+
+from pprint import pprint
+import pythonwhois  # i'm using this http://cryto.net/pythonwhois
+#domains = ['sindominio.net', 'ucm.es', 'google.com', 'stackoverflow.com']
+dom = 'sindominio.net'
+details = pythonwhois.get_whois(dom)
+#pprint(details)
+print(dom)
+print(details['expiration_date'])
+print(details['registrar'])
+print(details['nameservers'])    
+print(details['contacts']['registrant'])
