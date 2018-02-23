e91d28e9c1f503adcc1ca167780535e894622194
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-02-23 18:56:08 +0100

    webscraping-newspaper-multithreading.py: add spanish newspapers

--- newspaper/webscraping-newspaper-multithreading.py
+++ newspaper/webscraping-newspaper-multithreading.py
@@ -3,13 +3,19 @@
 
 import newspaper
 from newspaper import news_pool
+from pprint import pprint
 
-slate_paper = newspaper.build('http://slate.com')
-tc_paper = newspaper.build('http://techcrunch.com')
-espn_paper = newspaper.build('http://espn.com')
-
-papers = [slate_paper, tc_paper, espn_paper]
+# slate_paper = newspaper.build('http://slate.com')
+# tc_paper = newspaper.build('http://techcrunch.com')
+# espn_paper = newspaper.build('http://espn.com')
+elpais = newspaper.build('http://elpais.com')
+elmundo = newspaper.build('http://www.elmundo.es')
+publico = newspaper.build('http://www.publico.es')
+papers = [elpais, elmundo, publico]
 news_pool.set(papers, threads_per_source=2) # (3*2) = 6 threads total
 news_pool.join()
 
-print(slate_paper.articles[10].html)
+print(len(papers))
+pprint(papers)
+print(len(elpais.articles))
+print(len(elmundo.articles))
