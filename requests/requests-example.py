4cd168a6a5e2fd763fc6493c004dd64d896c6955
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-02-26 12:13:10 +0100

    to requests

--- requests/requests-example.py
+++ requests/requests-example.py
@@ -1,9 +1,9 @@
 #! python3
 #  Opens several Google search results.
-import requests, sys, webbrowser, bs4
+import requests, sys
 
 print('Googling...') # display text while downloading the Google page
 if len(sys.argv) > 1:
     search = ' '.join(sys.argv[1:])
     res = requests.get('http://google.com/search?q=' + search )
-    print res.text
+    print(res.text)
