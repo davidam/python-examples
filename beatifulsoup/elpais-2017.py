a2df17367f38e84ceb809999d149fcc81cc862f5
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-02-26 16:29:57 +0100

    elpais-2017.py: creation

---
+++ beatifulsoup/elpais-2017.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+from bs4 import BeautifulSoup
+import requests
+
+
+#https://elpais.com/hemeroteca/elpais/2017/01/01/m/portada.html
+
+list = []
+for i in range(1, 13):
+    if (i < 10):
+        i = "0" + str(i)
+    for j in range(1, 32):
+        if (j < 10):
+            j = "0" + str(j)
+        url = "https://elpais.com/hemeroteca/elpais/2017/"+str(i)+"/"+str(j)+"/m/portada.html"
+        list.append(url)
+        res = requests.get(url)
+        print("https://elpais.com/hemeroteca/elpais/2017/"+str(i)+"/"+str(j)+"/m/portada.html")
+        file = open("elpais-2017-"+str(i)+"-"+str(j)+".html", "w")
+        file.write(res.text) 
+
+        
+# url =  'http://www.gnu.org'
+# page = requests.get(url)
+# soup = BeatifulSoup(page.text, 'html.parser')
