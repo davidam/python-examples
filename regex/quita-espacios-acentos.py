dc60ae1fb25f98ad7f29036ee8515185488c6a77
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-02-27 15:03:43 +0100

    quita-espacios-acentos.py: creation

---
+++ regex/quita-espacios-acentos.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python# -*- coding: utf-8 -*-
+import re, unicodedata
+
+def elimina_tildes(s):
+    aux = ""
+    for c in unicodedata.normalize('NFD', s):
+        if (unicodedata.category(c) != 'Mn'):
+            aux = aux + c
+    return aux
+
+print(elimina_tildes("córcholis"))
+            
+def elimina_espacios(s):
+    result = ""
+    if re.search(r' ', s):
+        result = re.sub(r' ', '-', s)
+    return result
+
+def elimina_tildes_espacios(m):
+    aux = ""
+    aux = elimina_tildes(m)
+    aux = elimina_espacios(aux)
+    return aux
+
+cadena = "Hola Mundo"    
+print(elimina_espacios(cadena))
+
+cadena2 = "Jamón"
+print(elimina_espacios(cadena2))
+
+cadena3 = "Era un hombre diligente, la policía entró en su casa rápidamente"
+print(cadena3)
+print(elimina_tildes_espacios(cadena3))
+
