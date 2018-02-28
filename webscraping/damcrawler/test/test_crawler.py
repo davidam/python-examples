3437138f32f75b0ae898caec227cf098b8ded020
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-02-28 20:14:59 +0100

    test_crawler.py: replace crawler by formatter

--- webscraping/damcrawler/test/test_crawler.py
+++ webscraping/damcrawler/test/test_crawler.py
@@ -5,15 +5,16 @@ import unittest, os
 from app.crawler import Crawler
  
 class TddCrawler(unittest.TestCase):
-    def test_formatter_downloadInit_method_returns_correct_result(self):
+    def test_crawler_downloadInit_method_returns_correct_result(self):
         c = Crawler("http://www.gnu.org")
         self.assertEqual(c.url, "http://www.gnu.org")
         self.assertEqual(c.title, "The GNU Operating System and the Free Software Movement")        
 
-    def test_formatter_downloadOneUrl_method_returns_correct_result(self):
+    def test_crawler_downloadOneUrl_method_returns_correct_result(self):
         c = Crawler("http://www.urjc.es")
         c.downloadOneUrl("urjc.html")
         self.assertEqual(os.path.exists("urjc.html"), True)
-                
+            
+        
 if __name__ == '__main__':
     unittest.main()
