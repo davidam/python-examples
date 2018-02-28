c8227365dbe760a307add55f2b0f87a6ea4c482c
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-02-28 11:23:15 +0100

    test_crawler.py: add init test

--- webscraping/damcrawler/test/test_crawler.py
+++ webscraping/damcrawler/test/test_crawler.py
@@ -5,6 +5,11 @@ import unittest, os
 from app.crawler import Crawler
  
 class TddCrawler(unittest.TestCase):
+    def test_formatter_downloadInit_method_returns_correct_result(self):
+        c = Crawler("http://www.gnu.org")
+        self.assertEqual(c.url, "http://www.gnu.org")
+        self.assertEqual(c.title, "The GNU Operating System and the Free Software Movement")        
+
     def test_formatter_downloadOneUrl_method_returns_correct_result(self):
         c = Crawler("http://www.urjc.es")
         c.downloadOneUrl("urjc.html")
