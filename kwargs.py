230a7aad20da8cad63ad2c374c92f8fed142a4f0
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-02-28 20:02:13 +0100

    kwargs.py: creation

---
+++ kwargs.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def test_var_kwargs(farg, **kwargs):
+    print("formal arg:" + str(farg))
+    for key in kwargs:
+        print("another keyword arg: %s: %s" % (key, kwargs[key]))
+
+test_var_kwargs(farg=1, myarg2="two", myarg3=3)
