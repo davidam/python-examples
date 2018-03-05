cb63be9dc0b0e8f36df74a809c7d6c8c04a0bd6c
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-03-05 16:10:32 +0100

    urlthread.py: creation

---
+++ webscraping/damcrawler/app/urlthread.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import requests, threading
+from lxml import html
+
+class UrlThread(threading.Thread):
+
+    def __init__(self, s):
+        self.url = s
+        threading.Thread.__init__(self)        
+
+    def run(self):
+        response = requests.get(self.url)
+        tree = html.fromstring(response.text)
+        self.hrefs = tree.xpath('//a//@href')
+        self.title = tree.xpath('//title')[0].text_content()
+        self.content = response.text
+
+
+
+
