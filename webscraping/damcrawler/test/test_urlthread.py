ea356e2222b4d369f20308616346390090f7c12b
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-03-05 16:10:52 +0100

    test_urlthread.py: creation

---
+++ webscraping/damcrawler/test/test_urlthread.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import unittest, os, threading
+from app.urlthread import UrlThread
+ 
+class TddUrlThread(unittest.TestCase):
+    def test_urlthread_fetch_method_returns_correct_result(self):
+        f = UrlThread("http://www.gnu.org")
+        f.start()
+        f.join()
+        self.assertEqual(f.url, "http://www.gnu.org")
+        self.assertEqual(f.title, "The GNU Operating System and the Free Software Movement")        
+        self.assertEqual(len(f.hrefs)>1, True)
