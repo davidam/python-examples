4f9254a02c0a1ef2beb1fbb5d993d81dfabd5708
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-03-06 16:15:48 +0100

    kwargs.py: new print

--- kwargs.py
+++ kwargs.py
@@ -5,5 +5,7 @@ def test_var_kwargs(farg, **kwargs):
     print("formal arg:" + str(farg))
     for key in kwargs:
         print("another keyword arg: %s: %s" % (key, kwargs[key]))
+    if ("myarg2" in kwargs):
+        print(str(kwargs["myarg2"]))
 
 test_var_kwargs(farg=1, myarg2="two", myarg3=3)
