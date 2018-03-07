2b8124835dde22c8d480a4d54167613346cbeeb7
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-03-07 09:53:10 +0100

    test_urlthread.py: add fetch with timeout=15

--- webscraping/damcrawler/test/test_urlthread.py
+++ webscraping/damcrawler/test/test_urlthread.py
@@ -12,3 +12,11 @@ class TddUrlThread(unittest.TestCase):
         self.assertEqual(f.url, "http://www.gnu.org")
         self.assertEqual(f.title, "The GNU Operating System and the Free Software Movement")        
         self.assertEqual(len(f.hrefs)>1, True)
+        
+    def test_urlthread_fetch15_method_returns_correct_result(self):
+        f = UrlThread("http://www.gnu.org")
+        f.start()
+        f.join(timeout=15)
+        self.assertEqual(f.url, "http://www.gnu.org")
+        self.assertEqual(f.title, "The GNU Operating System and the Free Software Movement")        
+        self.assertEqual(len(f.hrefs)>1, True)
