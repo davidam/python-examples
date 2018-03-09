acade73087a9ecda598e6cf69cb05d7913e9b9b5
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-03-09 10:07:57 +0100

    range.py: useful range examples

--- range.py
+++ range.py
@@ -2,4 +2,15 @@
 
 a = ['Mary', 'had', 'a', 'little', 'lamb']
 for i in range(len(a)):
-    print i, a[i]
+    print(i, a[i])
+
+month = 13
+days = 32
+
+print('Give me days in a month')
+for i in range(1, days):
+    print(i)
+
+print('Give me months in a year')    
+for i in range(1, month):
+    print(i)
