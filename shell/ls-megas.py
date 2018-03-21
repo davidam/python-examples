6bcc38ad40ea14bcfb35645d0f7e1b607d7d3dba
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-03-21 10:55:58 +0100

    ls-megas.py: to python3

--- shell/ls-megas.py
+++ shell/ls-megas.py
@@ -27,7 +27,8 @@ for line in f:
     fields = line.strip().split()
     # Array indices start at 0 unlike AWK
     if (len(fields) == 2):
-        print line
+        print(line)
     else:
         megas = int(fields[4]) / 1024 / 1024
         print(fields[0] + " " + fields[1] + " " + fields[2] + " " + fields[3] + " " + str(megas) + " MB " + fields[5] + " " + fields[6] + " " + fields[7] + " " + fields[8])
+
