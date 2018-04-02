6a45fb3c6380d54a1da4fa2c5b19ac8f32cae0be
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-04-02 12:09:45 +0200

    argv-exec.py: to python3

--- shell/argv-exec.py
+++ shell/argv-exec.py
@@ -4,7 +4,7 @@ import sys
 
 if len(sys.argv) > 1:
     arg1 = sys.argv[1]
-    print arg1
+    print(arg1)
     subprocess.call(str(arg1), shell=True)
 else:
     print("You must give a command to execute")
