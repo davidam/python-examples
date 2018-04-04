26828d3407276ece2d9c6e9ddcd913f970ae1b2c
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-04-04 19:42:30 +0200

    php.py: to basics

--- php.py
+++
@@ -1,7 +0,0 @@
-import re  
-text = "PHP Exercises"  
-print("Original Text: ",text)  
-redata = re.compile(re.escape('php'), re.IGNORECASE)  
-new_text = redata.sub('Python', 'PHP Exercises')  
-print("Using 'php' replace PHP")   
-print("New Text: ",new_text) 
