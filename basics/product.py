10cb3ab588567159710ea1d750635f50deb82867
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-04-04 09:56:34 +0200

    move to basics

---
+++ basics/product.py
@@ -0,0 +1,8 @@
+from itertools import product
+
+print(list(product([1,2,3],repeat = 2)))
+print(list(product([1,2,3],[3,4])))
+A = [[1,2,3],[3,4,5]]
+print(list(product(*A)))
+B = [[1,2,3],[3,4,5],[7,8]]
+print(list(product(*B)))
