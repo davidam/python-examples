1ea20546b7cd6b2783c4a4f78ea4d1d799acb61f
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-04-04 12:17:05 +0200

    reduce.py: fix bugs and extend examples

--- functional/reduce.py
+++ functional/reduce.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python
-import functools
+from functools import reduce
+from functools import partial
 
-print reduce(lambda x, y: x+y, [1, 2, 3, 4, 5])
-
-test = ["1", "2", "3"]
-print reduce((lambda x,y: x+y), test, "testing")
+f = lambda a,b: a if (a > b) else b
+print("REDUCE EXAMPLES")
+print("a if (a > b) else b")
+print(reduce(f, [47,11,42,102,13]))
+print("x+y, range(1,101)")
+print(reduce(lambda x, y: x+y, range(1,101)))
+print("x*y, range(1,49)")
+print(reduce(lambda x, y: x*y, range(1,49)))
+print(reduce(lambda x, y: x*y, range(44,50))/reduce(lambda x, y: x*y, range(1,7)))
 
 def foo(a, b, c):
      return a + b if c else a * b
 
-print reduce(functools.partial(foo, c=True), [1,2,3,4,5], 2)
-print reduce(functools.partial(foo, c=False), [1,2,3,4,5], 2)
+print(reduce(partial(foo, c=True), [1,2,3,4,5], 2))
+print(reduce(partial(foo, c=False), [1,2,3,4,5], 2))
