6a9f6004529b52d9d5027e5ba41f7afe50b79ded
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-04-05 10:18:03 +0200

    togglebutton.py: fixes

--- gtk/togglebutton.py
+++ gtk/togglebutton.py
@@ -1,85 +1,33 @@
-#!/usr/bin/env python
+import gi
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk
 
-# example togglebutton.py
-
-import pygtk
-pygtk.require('2.0')
-import gtk
-
-class ToggleButton:
-    # Our callback.
-    # The data passed to this method is printed to stdout
-    def callback(self, widget, data=None):
-        print "%s was toggled %s" % (data, ("OFF", "ON")[widget.get_active()])
-
-    # This callback quits the program
-    def delete_event(self, widget, event, data=None):
-        gtk.main_quit()
-        return False
+class ToggleButtonWindow(Gtk.Window):
 
     def __init__(self):
-        # Create a new window
-        self.window = gtk.Window(gtk.WINDOW_TOPLEVEL)
+        Gtk.Window.__init__(self, title="ToggleButton Demo")
+        self.set_border_width(10)
 
-        # Set the window title
-        self.window.set_title("Toggle Button")
+        hbox = Gtk.Box(spacing=6)
+        self.add(hbox)
 
-        # Set a handler for delete_event that immediately
-        # exits GTK.
-        self.window.connect("delete_event", self.delete_event)
+        button = Gtk.ToggleButton("Button 1")
+        button.connect("toggled", self.on_button_toggled, "1")
+        hbox.pack_start(button, True, True, 0)
 
-        # Sets the border width of the window.
-        self.window.set_border_width(20)
+        button = Gtk.ToggleButton("B_utton 2", use_underline=True)
+        button.set_active(True)
+        button.connect("toggled", self.on_button_toggled, "2")
+        hbox.pack_start(button, True, True, 0)
 
-        # Create a vertical box
-        vbox = gtk.VBox(True, 2)
+    def on_button_toggled(self, button, name):
+        if button.get_active():
+            state = "on"
+        else:
+            state = "off"
+        print("Button", name, "was turned", state)
 
-        # Put the vbox in the main window
-        self.window.add(vbox)
-
-        # Create first button
-        button = gtk.ToggleButton("toggle button 1")
-
-        # When the button is toggled, we call the "callback" method
-        # with a pointer to "button" as its argument
-        button.connect("toggled", self.callback, "toggle button 1")
-
-
-        # Insert button 1
-        vbox.pack_start(button, True, True, 2)
-
-        button.show()
-
-        # Create second button
-
-        button = gtk.ToggleButton("toggle button 2")
-
-        # When the button is toggled, we call the "callback" method
-        # with a pointer to "button 2" as its argument
-        button.connect("toggled", self.callback, "toggle button 2")
-        # Insert button 2
-        vbox.pack_start(button, True, True, 2)
-
-        button.show()
-
-        # Create "Quit" button
-        button = gtk.Button("Quit")
-
-        # When the button is clicked, we call the main_quit function
-        # and the program exits
-        button.connect("clicked", lambda wid: gtk.main_quit())
-
-        # Insert the quit button
-        vbox.pack_start(button, True, True, 2)
-
-        button.show()
-        vbox.show()
-        self.window.show()
-
-def main():
-    gtk.main()
-    return 0       
-
-if __name__ == "__main__":
-    ToggleButton()
-    main()
+win = ToggleButtonWindow()
+win.connect("destroy", Gtk.main_quit)
+win.show_all()
+Gtk.main()
