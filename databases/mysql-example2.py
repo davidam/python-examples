ca4efb7c366761d27bea027d21d8b7f60468595f
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-04-09 17:35:32 +0200

    mysqldb.py to databases/mysql-example2.py

--- databases/mysql-example2.py
+++ databases/mysql-example2.py
@@ -2,19 +2,19 @@
 import MySQLdb
 
 db = MySQLdb.connect(host="localhost",    # your host, usually localhost
-                     user="user",         # your username
-                     passwd="pass",  # your password
-                     db="openatrium")        # name of the data base
+                     user="root",         # your username
+                     passwd="lala",  # your password
+                     db="sqlexamples")        # name of the data base
 
 # you must create a Cursor object. It will let
 #  you execute all the queries you need
 cur = db.cursor()
 
 # Use all the SQL you like
-cur.execute("SELECT mail FROM users")
+cur.execute("SELECT b1 FROM b")
 
 # print all the first cell of all the rows
 for row in cur.fetchall():
-    print row[0]
+    print(row[0])
 
 db.close()
