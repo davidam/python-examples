fa3a377ca5a266cc40a23fb15797cd3a8d0e3e69
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-04-09 11:47:58 +0200

    netcdf-example4.py: to python3

--- netcdf/netcdf-example4.py
+++ netcdf/netcdf-example4.py
@@ -5,18 +5,18 @@ rootgrp = Dataset("test.nc", "a")
 fcstgrp = rootgrp.createGroup("forecasts")
 analgrp = rootgrp.createGroup("analyses")
 
-print rootgrp.groups
+print(rootgrp.groups)
 
 level = rootgrp.createDimension("level", None)
 time = rootgrp.createDimension("time", None)
 lat = rootgrp.createDimension("lat", 73)
 lon = rootgrp.createDimension("lon", 144)
 
-print rootgrp.dimensions
+print(rootgrp.dimensions)
 
-print len(lon)
-print lon.isunlimited()
-print time.isunlimited()
+print(len(lon))
+print(lon.isunlimited())
+print(time.isunlimited())
 
 for dimobj in rootgrp.dimensions.values():
-    print dimobj
+    print(dimobj)
