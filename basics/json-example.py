795c8b2fcf3d27f15429191e76d52cc78c1c15ba
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-04-10 16:01:51 +0200

    json-example.py: fixes

--- basics/json-example.py
+++ basics/json-example.py
@@ -1,10 +1,10 @@
 import json
 json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
-print json.dumps("\"foo\bar")
-print json.dumps(u'\u1234')
-print json.dumps('\\')
-print json.dumps({"c": 0, "b": 0, "a": 0}, sort_keys=True)
-from StringIO import StringIO
-io = StringIO()
-json.dump(['streaming API'], io)
-io.getvalue()
+print(json.dumps("\"foo\bar"))
+print(json.dumps(u'\u1234'))
+print(json.dumps('\\'))
+print(json.dumps({"c": 0, "b": 0, "a": 0}, sort_keys=True))
+# from StringIO import StringIO
+# io = StringIO()
+# json.dump(['streaming API'], io)
+# io.getvalue()
