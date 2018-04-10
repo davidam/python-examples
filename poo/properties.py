402d4535d7895efb513d0a53ac64de2e489fbcbd
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-04-10 10:44:53 +0200

    properties.py: creation

---
+++ poo/properties.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python# -*- coding: utf-8 -*-
+class Celsius:
+    def __init__(self, temperature = 0):
+        self.temperature = temperature
+
+    def to_fahrenheit(self):
+        return (self.temperature * 1.8) + 32
+
+    def get_temperature(self):
+        print("Getting value")
+        return self._temperature
+
+    def set_temperature(self, value):
+        if value < -273:
+            raise ValueError("Temperature below -273 is not possible")
+        print("Setting value")
+        self._temperature = value
+
+    temperature = property(get_temperature,set_temperature)
+
+c = Celsius()
+
+c.temperature
+c.temperature = 37
+print(c.to_fahrenheit())
+     
