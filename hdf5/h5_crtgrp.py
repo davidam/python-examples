ad3133da5e7022c83f731bca707a26601aa97e50
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-04-10 07:40:09 +0200

    to python3

--- hdf5/h5_crtgrp.py
+++ hdf5/h5_crtgrp.py
@@ -11,25 +11,25 @@ import h5py
 # Use 'w' to remove existing file and create a new one; use 'w-' if
 # create operation should fail when the file already exists.
 #
-print "Creating an HDF5 file with the name group.h5..."
+print("Creating an HDF5 file with the name group.h5...")
 file = h5py.File('group.h5','w')
 #
 # Show the Root group which is created when the file is created.
 #
-print "When an HDF5 file is created, it has a Root group with the name '",file.name,"'."
+print("When an HDF5 file is created, it has a Root group with the name '",file.name,"'.")
 #
 # Create a group with the name "MyGroup"
 #
-print "Creating a group MyGroup in the file..."
+print("Creating a group MyGroup in the file...")
 group = file.create_group("MyGroup")
 # 
-# Print the content of the Root group
+# Print(the content of the Root group
 #
-print "An HDF5 group is a container for other objects; a group is similar to Python dictionary with the keys being the links to the group members."
-print "Show the members of the Root group using dictionary key method:", file.keys()
+print("An HDF5 group is a container for other objects; a group is similar to Python dictionary with the keys being the links to the group members.")
+print("Show the members of the Root group using dictionary key method:", file.keys())
 #
 # Another way to show the content of the Root group.
-print "Show the members of the Root group using the list function:", list(file)
+print("Show the members of the Root group using the list function:", list(file))
 #
 # Close the file before exiting; H5Py will close the group.
 #
