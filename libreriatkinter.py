a7199563cff052d9f9b0f1b3b983e34f6ed1689a
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-04-10 07:21:18 +0200

    move to tkinter

--- libreriatkinter.py
+++
@@ -1,9 +0,0 @@
-try:
-    # for Python2
-    from Tkinter import *   ## notice capitalized T in Tkinter 
-except ImportError:
-    # for Python3
-    from tkinter import *   ## notice lowercase 't' in tkinter here
-
-ventana = Tk()
-ventana.mainloop()
