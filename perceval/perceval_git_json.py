79b1ec74852232a4bc4b5a2214a846ba1c6d5f30
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-04-19 17:30:22 +0200

    perceval_git_json.py: creation

---
+++ perceval/perceval_git_json.py
@@ -0,0 +1,29 @@
+#! /usr/bin/env python3
+# Count commits
+# You can execute this script following this tutorial:
+# https://grimoirelab.gitbooks.io/training/perceval/first_steps.html
+# source ~/venvs/perceval/bin/activate 
+# $ python3 ~/git/python-examples/perceval/perceval_git_counter.py https://github.com/grimoirelab/perceval.git /tmp/clonedir
+
+import argparse
+
+from perceval.backends.core.git import Git
+from pprint import pprint
+# Read command line arguments
+parser = argparse.ArgumentParser(description = "Count commits in a git repo")
+parser.add_argument("repo", help = "Repository url")
+parser.add_argument("dir", help = "Directory for cloning the repository")
+parser.add_argument("--print", action='store_true', help = "Print hashes")
+args = parser.parse_args()
+
+# create a Git object, and count commmits
+repo = Git(uri=args.repo, gitpath=args.dir)
+count = 0
+for commit in repo.fetch():
+    pprint(commit)
+
+    if args.print:
+        print(commit['data']['commit'])
+    count += 1
+
+print("Number of commmits: %d." % (count))
