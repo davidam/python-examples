95c3d2d47e3137a16dcb443c9a187e24e7a27311
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-04-20 05:12:09 +0200

    allindices.py: to basics

---
+++ basics/allindices.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python# -*- coding: utf-8 -*-
+aList = [123, 'xyz', 'zara', 'abc'];
+print("Index for xyz : %s" % aList.index( 'xyz' ))
+print("Index for zara : %s" % aList.index( 'zara' ))
+#all_indices("foo", ["foo","bar","baz","foo"])
