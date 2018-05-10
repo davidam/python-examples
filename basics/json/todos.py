68d4e9d6c4f6941d49a7fc7ed38120c768de8b8e
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-05-10 10:58:02 +0200

    todos.py: creation

---
+++ basics/json/todos.py
@@ -0,0 +1,39 @@
+import json
+import requests
+
+response = requests.get("https://jsonplaceholder.typicode.com/todos")
+todos = json.loads(response.text)
+
+print("todos[:10]")
+print(todos[:10])
+
+# Map of userId to number of complete TODOs for that user
+todos_by_user = {}
+
+# Increment complete TODOs count for each user.
+for todo in todos:
+    if todo["completed"]:
+        try:
+            # Increment the existing user's count.
+            todos_by_user[todo["userId"]] += 1
+        except KeyError:
+            # This user has not been seen. Set their count to 1.
+            todos_by_user[todo["userId"]] = 1
+print("todos_by_user")                        
+print(todos_by_user)            
+# Create a sorted list of (userId, num_complete) pairs.
+top_users = sorted(todos_by_user.items(), 
+                   key=lambda x: x[1], reverse=True)
+# Get the maximum number of complete TODOs.
+max_complete = top_users[0][1]
+# Create a list of all users who have completed
+# the maximum number of TODOs.
+users = []
+for user, num_complete in top_users:
+    if num_complete < max_complete:
+        break
+    users.append(str(user))
+print("users")    
+print(users)    
+# max_users = " and ".join(users)
+
