9f3616babd95d7b37560c24a3abc204e45402156
David Arroyo <davidam@gmail.com>
2018-06-02 15:02:18 +0200

    comebebes.py: bebes in an integer number

--- games/comebebes.py
+++ games/comebebes.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python
 
 bebes = input("Give me the number of babies: ")
+bebes = int(bebes)
+
 if bebes == 1:
     print("I've eaten " + str(bebes) + " baby.")
 else:
     print("I've eaten " + str(bebes) + " babies.")
-
+    
 if bebes > 100:
     print("I'm Edelweiss")
