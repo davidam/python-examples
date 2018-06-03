ec136b97723b14bc12627c0099862e921952af51
David Arroyo <davidam@gmail.com>
2018-06-03 16:41:08 +0200

    groupkfold.py: creation

---
+++ scikit/groupkfold.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+# Boston, MA 02110-1301 USA,
+
+from sklearn.model_selection import GroupKFold
+
+X = [0.1, 0.2, 2.2, 2.4, 2.3, 4.55, 5.8, 8.8, 9, 10]
+y = ["a", "b", "b", "b", "c", "c", "c", "d", "d", "d"]
+groups = [1, 1, 1, 2, 2, 2, 3, 3, 3, 3]
+
+gkf = GroupKFold(n_splits=3)
+for train, test in gkf.split(X, y, groups=groups):
+    print("train: %s, test: %s" % (train, test))
