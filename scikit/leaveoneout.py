9ccce7b8902165d31ed9f969d67bd82c72bffee8
David Arroyo <davidam@gmail.com>
2018-06-03 16:39:28 +0200

    leaveoneout.py: creation

---
+++ scikit/leaveoneout.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+# Boston, MA 02110-1301 USA,
+
+from sklearn.model_selection import LeaveOneOut
+import numpy as np
+
+X = np.array([[1, 2], [3, 4]])
+y = np.array([1, 2])
+loo = LeaveOneOut()
+loo.get_n_splits(X)
+
+print(loo)
+
+for train_index, test_index in loo.split(X):
+   print("TRAIN INDEX:", train_index, "TEST INDE:", test_index)
+   X_train, X_test = X[train_index], X[test_index]
+   y_train, y_test = y[train_index], y[test_index]
+   print(X_train, X_test, y_train, y_test)
