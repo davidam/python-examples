7bc9aa6611f27ad08b8373d12121e4704eed3edf
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-06-22 17:01:24 +0200

    cracklepop.py: to python3

--- games/cracklepop.py
+++ games/cracklepop.py
@@ -2,10 +2,10 @@
 
 for i in range(1, 101):
     if ((i % 3) == 0) and ((i % 5) == 0): 
-        print "cracklepop"
+        print("cracklepop")
     if (i % 3) == 0: 
-        print "crackle"
+        print("crackle")
     elif (i % 5) == 0:
-        print "pop"
+        print("pop")
     elif ((i % 3) != 0) | ((i % 5) != 0):
-        print i
+        print(i)
