d8834b6c7c80af1167f88584d4267cfe14d25809
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-06-24 12:41:35 +0200

    boxplot-example2.py: creation

---
+++ matplotlib/boxplot-example2.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# fake data
+np.random.seed(937)
+data = np.random.lognormal(size=(37, 4), mean=1.5, sigma=1.75)
+labels = list('ABCD')
+fs = 10  # fontsize
+
+# demonstrate how to toggle the display of different elements:
+fig, axes = plt.subplots(nrows=2, ncols=3, figsize=(6, 6), sharey=True)
+axes[0, 0].boxplot(data, labels=labels)
+axes[0, 0].set_title('Default', fontsize=fs)
+
+fig.subplots_adjust(hspace=0.4)
+plt.show()
