845e0a0603df0b6460da86dbd4741b13d9a4f5d6
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-06-24 17:06:36 +0200

    mysql-example2.py: interactive password

--- databases/mysql-example2.py
+++ databases/mysql-example2.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python
 import MySQLdb
+import getpass
+DB_PASS = getpass.getpass("Give me your MySQL password: ")
 
 db = MySQLdb.connect(host="localhost",    # your host, usually localhost
                      user="root",         # your username
-                     passwd="lala",  # your password
+                     passwd=DB_PASS,  # your password
                      db="sqlexamples")        # name of the data base
 
 # you must create a Cursor object. It will let
