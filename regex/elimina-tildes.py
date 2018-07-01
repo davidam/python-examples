856fc70945aed74161dc33366291e3d7beec4980
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-07-01 14:59:40 +0200

    elimina-tildes.py: add print

--- regex/elimina-tildes.py
+++ regex/elimina-tildes.py
@@ -5,6 +5,7 @@ import unicodedata
 def elimina_tildes(s):
    return ''.join((c for c in unicodedata.normalize('NFD', s) if unicodedata.category(c) != 'Mn'))
 
+print("córcholis")
 print(elimina_tildes("córcholis"))
 
 def elimina_tildes2(s):
