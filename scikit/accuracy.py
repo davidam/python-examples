d222918dac83d55cafd14884f621f66181b0ab13
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-07-03 16:41:50 +0200

    accuracy.py: add prints

--- scikit/accuracy.py
+++ scikit/accuracy.py
@@ -7,16 +7,21 @@ from sklearn.metrics import accuracy_score
 
 # True class
 y = [0, 0, 1, 1, 0]
+print("True class: %s" % y)
+
 # Predicted class
 y_hat = [0, 1, 1, 0, 0]
-
+print("Predicted class: %s" % y_hat)
 # 60% accuracy
-print(accuracy_score(y, y_hat))
+print("Accuracy: %s" % accuracy_score(y, y_hat))
 
 # True class
 y = [0, 1, 2, 1, 0]
+print("True class: %s" % y)
 # Predicted class
 y_hat = [0, 2, 2, 1, 0]
+print("Predicted class: %s" % y_hat)
 
 # 80% accuracy
-print(accuracy_score(y, y_hat))
+print("Accuracy: %s" % accuracy_score(y, y_hat))
+
