e8cc3b1065494669437f771c5ee47558a8ad699b
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-07-04 19:04:18 +0200

    stopwords.py: creation

--- nlp/nltk/stopwords.py
+++ nlp/nltk/stopwords.py
@@ -5,8 +5,8 @@ print(stopwords.words('english'))
 print(stopwords.words('spanish'))
 
 def content_fraction(text):
-    stopwords = nltk.corpus.stopwords.words('english')
+    stopwords = nltk.corpus.stopwords.words('spanish')
     content = [w for w in text if w.lower() not in stopwords]
     return len(content) / len(text)
 
-
+print("The content fraction of the sentence is: %s" % content_fraction("En un lugar de la Mancha de cuyo nombre no quiero acordarme"))
