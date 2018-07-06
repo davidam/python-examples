822e0048df23fabd65467e2fa3e6997b02b6d32a
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-07-06 10:38:36 +0200

    scipy-cosine-similarity.py: creation

---
+++ scipy/scipy-cosine-similarity.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+# Boston, MA 02110-1301 USA,
+
+from scipy import spatial
+
+dataSetI = [3, 45, 7, 2]
+dataSetII = [2, 54, 13, 15]
+print("The distance is %s: " % spatial.distance.cosine(dataSetI, dataSetII))
+print("The cosine similarity is %s: " % (1 - spatial.distance.cosine(dataSetI, dataSetII)))
