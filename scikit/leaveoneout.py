a470609cca6f8ec69b7a863b90b325a7702080e1
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-07-06 11:07:07 +0200

    leaveoneout.py: small improvement

--- scikit/leaveoneout.py
+++ scikit/leaveoneout.py
@@ -27,12 +27,10 @@ import numpy as np
 X = np.array([[1, 2], [3, 4]])
 y = np.array([1, 2])
 loo = LeaveOneOut()
-loo.get_n_splits(X)
-
-print(loo)
+print("loo.get_n_splits(X) %s:" % loo.get_n_splits(X))
 
 for train_index, test_index in loo.split(X):
-   print("TRAIN INDEX:", train_index, "TEST INDE:", test_index)
+   print("TRAIN INDEX:", train_index, "TEST INDEX:", test_index)
    X_train, X_test = X[train_index], X[test_index]
    y_train, y_test = y[train_index], y[test_index]
    print(X_train, X_test, y_train, y_test)
