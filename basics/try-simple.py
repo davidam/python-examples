87aa962c24014aa7c03ed13c66224b4e9d38c9aa
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-07-07 15:54:04 +0200

    try-simple.py: some fixes

--- basics/try-simple.py
+++ basics/try-simple.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
 import os
 import signal
 
@@ -13,33 +34,32 @@ except FileNotFoundError:
 
 print("The program has found the file, it continues.")
 
-a = [1, 2, 3]
-# a[0] = str(input("Give me an element for my array: "))
-# a[1] = str(input("Give me another element for my array: "))
-# a[2] = str(input("Give me the last element for my array: "))
+a = []
+a.append(int(input("Give me an element for my array: ")))
+a.append(int(input("Give me another element for my array: ")))
+a.append(int(input("Give me the last element for my array: ")))
 
 try:
     print("Second element = %d" %(a[1]))
     # Throws error since there are only 3 elements in array
     print("Third element = %d" %(a[2]))
 except IndexError:
-    print("Your array has not the index")
+    print("The program has troubles with the array indexes")
     os.kill(os.getpid(), signal.SIGUSR1)
-
 
 print("The program don't have a trouble with the array indexes")
 
 # Program to handle multiple errors with one except statement
+n1 = int(input("Give me a number: "))
+n2 = int(input("Give me another number: "))
+
 try :
-    a = int(input("Give me an integer: "))
-    b = int(input("Give me another integer: "))
-    # throws ZeroDivisionError for a = 3
-    c = a/b
+    m = n1 / n2
     # throws NameError if a >= 4
-    print("Value of c = %s" % c)
+    print("Value of m = %s" % m)
 # note that braces () are necessary here for multiple exceptions
 except(ZeroDivisionError, NameError):
-    print("\nDividing % by %s has troubles with the zero division" % (a, b))
+    print("\nThe program has troubles with the zero division")
     os.kill(os.getpid(), signal.SIGUSR1)
 
-print("The program has not troubles with the zero division")
+print("The programa has not troubles with the zero division")
