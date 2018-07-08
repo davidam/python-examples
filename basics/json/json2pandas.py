4a984f14d7151e12b2b454f01a34b5e943f753b4
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-07-08 09:08:32 +0200

    json2pandas.py: creation

---
+++ basics/json/json2pandas.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import json
+from pprint import pprint
+import pandas as pd
+from pandas.io.json import json_normalize
+data = [
+  {
+    'name': {
+      'first': 'vikash',
+      'last': 'singh'
+    },
+    'age': 27
+  },
+  {
+    'name': {
+      'first': 'satyam',
+      'last': 'singh'
+    },
+    'age': 14
+  }
+]
+
+df = pd.DataFrame.from_dict(json_normalize(data), orient='columns')
+print(df)
