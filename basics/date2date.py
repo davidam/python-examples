fa88e69a05e67764962478e92e919a8e4c2c2e06
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-07-10 09:15:15 +0200

    date2date.py, datetoday.py: creation

---
+++ basics/date2date.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import datetime
+
+mydate = datetime.date(1943,3, 13)  #year, month, day
+print(mydate.strftime("%A"))
+print(mydate.strftime("%B"))
+
+from datetime import timedelta
+seconds=153.1205153
+d = timedelta(seconds)
+print(d)
+print(d.days)
+
+from datetime import date
+d2 = date.fromordinal(1234124)
+print(d2.strftime("%Y/%m/%d"))
+
+# dates = datetime.date(seconds)
+# print(date.strftime("%Y-%m-%d"))
---
+++ basics/datetoday.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import time
+import datetime
+
+print("Time in seconds since the epoch: %s" %time.time())
+print("Current date and time: " , datetime.datetime.now())
+print("Or like this: " ,datetime.datetime.now().strftime("%y-%m-%d-%H-%M"))
+
+
+print("Current year: ", datetime.date.today().strftime("%Y"))
+print("Month of year: ", datetime.date.today().strftime("%B"))
+print("Week number of the year: ", datetime.date.today().strftime("%W"))
+print("Weekday of the week: ", datetime.date.today().strftime("%w"))
+print("Day of year: ", datetime.date.today().strftime("%j"))
+print("Day of the month : ", datetime.date.today().strftime("%d"))
+print("Day of week: ", datetime.date.today().strftime("%A"))
