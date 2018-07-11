4c9427deaa5640fdf1bd4825df7cb17d45f1b172
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-07-11 11:50:27 +0200

    file-write.py: add writelines

--- basics/file-write.py
+++ basics/file-write.py
@@ -3,7 +3,8 @@
 
 file = open("testfile.txt", "w")
 
-file.write("This is a test") 
-file.write("To add more lines.")
-
+file.write("This is a test\n")
+file.write("To add more lines.\n")
+lines_of_text = ["One line of text here\n", "and another line here\n", "and yet another here\n", "and so on and so forth\n"]
+file.writelines(lines_of_text)
 file.close()
