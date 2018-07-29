67728d423566e4cf831ad21ad65b54bd56e6e67e
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-07-29 23:51:01 +0200

    unedsex.py: creation

---
+++ games/unedsex.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+print("Todas las universidades de España dicen DAMe, nosotros dependemos directamente del MEC al igual que CRUE(l)")
+print("Soy una indígena vasca")
+i = input("¿Te gusto? ( S | N ): ")
+
+if (i == 'S'):
+    print("El sentimiento vasco está fuertemente reprimido por la derecha rancia, el opus dei, etc.")
+    print("Solo se puede transmitir sentimiento algo sincero vasco, a través del sexo.")
+    s = input("¿Te interesa? ( S | N ): ")
+    if (s == 'S'):
+        print("Tendrás que aparecer en artículos, apoyar la estrategia de Euskal Herria para toda la vida, ...")
+        eu = input("¿Te interesa? ( S | N ): ")
+        if (eu == 'S'):
+            for i in range(1, 9):
+                print("year ", i)
+                print("writing source code")
+                print("writing source code")
+                print("writing source code")
+                print("writing source code")
+                print("publishing article")
+        print("Yo contenta estar, en ciudad científica la mano debes pedir, necesario para hijos tener. Universidad trabajamos. Jefes CRUE deben querer.")
+        ciudad = input("¿Quieres viajar a ciudad mágica? ( S | N ):")
+        if (ciudad == 'S'):
+            print("Pensamiento oscuro en casa blanca entra. Científico de buena raza ya no tiene casa, encerrado debes estar")
+            tr = input("¿Te tragas el discurso? ( S | N ):")
+            print("Da igual, se ha generado un trato CRUE contra ti, como no tienes delito creamos un discurso y al psiquiátrico vas")
+            print("Psiquiatra pregunta discurso")
+            uno = input("1. Soy un GNU, mi hembra me hizo así, tengo cuernos. ( S | N ):")
+            dos = input("2. Soy anarquista, soy peligroso, la sociedad me odia. ( S | N ):")
+            tres = input("3. En la ciudad mágica me llevaron a Delhais, todos envidiais mi gran poder actual. ( S | N ):")
+            cuatro = input("4. Mi hermano me violó de pequeño. ( S | N ):")
+            cinco = input("5. Las putas me joden el cerebro. ( S | N ):")
+            seis = input("6. Mi hermano quiere mi dinero, de mi herencia y está dispuesto a psiquiatría, ETA, fascismo, lo que haga falta ( S | N )")
+            if ( uno == 'S' ):
+                print("Algo aciertas, no creas animadversión a las personas que te han encerrado, solo a la hembra, todos los psicólogos y psiquiatras desde Freud coinciden en que los problemas de salud mental tienen que ver con el sexo")
+            if ( dos == 'S' ):
+                print("Has dicho un dato falso, tu nunca has estado en un grupo anarquista. ¿Por qué mientes?")
+            if ( tres == 'S' ):
+                print("El psiquiatra lee los periódicos y no aparecen extraterrestres. Eres loco.")
+            if ( cuatro == 'S' ):
+                print("El psiquiatra deberá negociar con tu violador tu libertad, tu verás.")
+            if ( cinco == 'S' ):
+                print("Contradices a Freud, el sexo ayuda a la psicología.")
+            if ( seis == 'S' ):
+                print("El psiquiatra no va a desmontar ni la ETA, ni el fascismo en España eso que lo sepas, tampoco va a arriesgar su vida, ni su puesto de trabajo, pero te escucha ¿piensas que te vale para algo?")
