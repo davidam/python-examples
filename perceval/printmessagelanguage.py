d1e4cfe9de06f02bede9bc042d279e2643bdfd95
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-07-30 21:35:15 +0200

    printmessagelanguage.py: creation

---
+++ perceval/printmessagelanguage.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import sys
+from nltk import wordpunct_tokenize
+from nltk.corpus import stopwords
+import json
+from pprint import pprint
+#from langdetect import detect
+from polyglot.detect import Detector
+
+if __name__=='__main__':
+
+    jsondata = open('perceval.json').read()
+    json_object = json.loads(jsondata)
+    #json = json_object.dumps()
+    perceval = json_object["perceval"]
+    pprint(json_object)
+    print(
+    "=======================================================================================" "\n"
+    "     Message  Language   \n"
+    "   ---------- ---------")
+
+for i in perceval:
+    detector = Detector(i['data']['message'])
+    print("     %s            %s  " % (i['data']['message'], detector.language))
+
+
+print("Take care with the json format!!")
