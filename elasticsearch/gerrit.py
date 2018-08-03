ec2c63bf6bf067e6a7977e3b915dbd36f931f862
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-08-03 17:24:04 +0200

    gerrit.py: add scatter objects

--- elasticsearch/gerrit.py
+++ elasticsearch/gerrit.py
@@ -14,3 +14,16 @@ filter_date_1y = Q('range', date={'gt': INIT_DATE_1Y, 'lt': END_DATE})
 
 print(filter_date_4y)
 print(filter_date_1y)
+
+from plotly.graph_objs import *
+trace0 = Scatter(
+    x=[1, 2, 3, 4],
+    y=[10, 15, 13, 17]
+)
+trace1 = Scatter(
+    x=[1, 2, 3, 4],
+    y=[16, 5, 11, 9]
+)
+data = Data([trace0, trace1])
+print(data)
+#iplot(data)
