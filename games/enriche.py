cbc21538327ff13b926766a81c54351455419412
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-08-04 15:38:36 +0200

    enriche.py: creation

---
+++ games/enriche.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+print("Soy Enrich-e, de la hoz y no el martillo")
+print("Este es mi programa enriqueceré España, los españoles, el e-learning, la e-cultura, ...")
+print("Y todo sin trabajar porque el trabajo mata")
+cifra = input("Cuéntame tu conflicto laboral, cuanto dinero hay en juego, pon una cifra: ")
+print("Enriche gana " + str(cifra) + " España se está levantando y tu también con tu barrio y sin trabajo!!")
