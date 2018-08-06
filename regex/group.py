f6866a26e29e9f4d41c4abe6f3e2861409f89d18
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-08-06 18:55:03 +0200

    group.py: more examples

--- regex/group.py
+++ regex/group.py
@@ -5,3 +5,13 @@ print(m.group(0))
 print(m.group(1))
 print(m.group(2))
 print(m.group(1, 2, 3))
+
+n = re.match(r"([[a-z]*]) (\w+) (\w+)", "[asf] tests Hola")
+print(n.group(0))
+print(n.group(1))
+print(n.group(2))
+
+q = re.match(r"([[a-z]*]) (.*)", "[asf] tests Hola")
+print(q.group(0))
+print(q.group(1))
+print(q.group(2))
