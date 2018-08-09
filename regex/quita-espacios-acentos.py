7d7a6da91e7649582b468ccb30eb31d3ef6e838d
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-08-09 10:32:15 +0200

    quita-espacios-acentos.py: add elimina_tildes method

--- regex/quita-espacios-acentos.py
+++ regex/quita-espacios-acentos.py
@@ -9,7 +9,14 @@ def elimina_tildes(s):
     return aux
 
 print(elimina_tildes("córcholis"))
-            
+
+def elimina_tildes(s):
+    aux = ""
+    for c in unicodedata.normalize('NFD', s):
+        if (unicodedata.category(c) != 'Mn'):
+            aux = aux + c
+    return aux
+
 def elimina_espacios(s):
     result = ""
     if re.search(r' ', s):
@@ -22,7 +29,7 @@ def elimina_tildes_espacios(m):
     aux = elimina_espacios(aux)
     return aux
 
-cadena = "Hola Mundo"    
+cadena = "Hola Mundo"
 print(elimina_espacios(cadena))
 
 cadena2 = "Jamón"
@@ -31,4 +38,3 @@ print(elimina_espacios(cadena2))
 cadena3 = "Era un hombre diligente, la policía entró en su casa rápidamente"
 print(cadena3)
 print(elimina_tildes_espacios(cadena3))
-
