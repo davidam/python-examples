4ed91301f8ad07513fcea4a74545ed9dac3c7fe8
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-08-13 13:37:18 +0200

    for.py: to python3

--- basics/for.py
+++ basics/for.py
@@ -4,22 +4,22 @@ words = ['cat', 'window', 'defenestrate']
 
 print("######################## First for: #######################")
 for w in words:
-    print w, len(w)
+    print(w, len(w))
 
 print("######################## Second for: #####################")
-    
+
 for i,w in enumerate(words):
-    print i, w
+    print(i, w)
 
 print("######################## Third for: #####################")
-    
+
 for i in range(1,10):
-    print i
-    
+    print(i)
+
 print("######################## Fourth for: #####################")
-    
+
 print(','.join('{}'.format(i) for i in range(1, 100, 4)))
-    
+
 print("######################## Fifth for: #####################")
 
 print(','.join('{},{}'.format(i, i + 1) for i in range(1, 100, 4)))
