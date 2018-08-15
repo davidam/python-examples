8edb5aa0d4db29279b53c7943247c087b60dd697
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-08-15 12:50:54 +0200

    scikit-naivebayes.py: to python3

--- scikit/scikit-naivebayes.py
+++ scikit/scikit-naivebayes.py
@@ -9,9 +9,9 @@ y = np.array([3, 3, 3, 3, 4, 3, 3, 4, 3, 4, 4, 4])
 #Create a Gaussian Classifier
 model = GaussianNB()
 
-# Train the model using the training sets 
+# Train the model using the training sets
 model.fit(x, y)
 
-#Predict Output 
+#Predict Output
 predicted= model.predict([[1,2],[3,4]])
-print predicted
+print(predicted)
