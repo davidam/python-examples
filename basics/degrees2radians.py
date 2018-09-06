6f7d9d074c486026794bcad8b0192a1f61c20392
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-09-06 10:42:39 +0200

    degrees2radians.py: creation

---
+++ basics/degrees2radians.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import math
+print(math.degrees(0))                       #0 radians == 0 degrees
+print(math.degrees(math.pi/2))               #pi/2 radians is 90 degrees
+print(math.degrees(math.pi))                 #pi radians is 180 degrees
+print(math.degrees(math.pi+(math.pi/2)))     #pi+pi/2 radians is 270 degrees
+print(math.degrees(math.pi+math.pi))         #2*pi radians is 360 degrees
