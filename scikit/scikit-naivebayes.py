8623d2ce74c659f6ad88c43624b3478e0bb2f59b
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-09-10 09:48:21 +0200

    scikit-naivebayes.py: add the three naive bayes implementations in scikit

--- scikit/scikit-naivebayes.py
+++ scikit/scikit-naivebayes.py
@@ -1,6 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+################# GAUSSIAN NAIVE BAYES ####################################
+
 #Import Library of Gaussian Naive Bayes model
 from sklearn.naive_bayes import GaussianNB
 import numpy as np
+
+print("Gaussian Naive Bayes model")
 
 #assigning predictor and target variables
 x= np.array([[-3,7],[1,5], [1,2], [-2,0], [2,3], [-4,0], [-1,1], [1,1], [-2,2], [2,7], [-4,1], [-2,7]])
@@ -15,3 +42,33 @@ model.fit(x, y)
 #Predict Output
 predicted= model.predict([[1,2],[3,4]])
 print(predicted)
+
+# With dataset
+from sklearn import datasets
+iris = datasets.load_iris()
+#print(iris)
+from sklearn.naive_bayes import GaussianNB
+gnb = GaussianNB()
+y_pred = gnb.fit(iris.data, iris.target).predict(iris.data)
+print("Number of mislabeled points out of a total %d points : %d"  % (iris.data.shape[0],(iris.target != y_pred).sum()))
+
+########################## MULTINOMIAL NAIVE BAYES ###########################
+print("Multinomial Naive Bayes model")
+import numpy as np
+X = np.random.randint(5, size=(6, 100))
+y = np.array([1, 2, 3, 4, 5, 6])
+from sklearn.naive_bayes import MultinomialNB
+clf = MultinomialNB()
+clf.fit(X, y)
+print(clf.predict(X[2:3]))
+
+########################## BERNOULLI NAIVE BAYES ###########################
+
+print("Bernoulli Naive Bayes model")
+import numpy as np
+X = np.random.randint(5, size=(6, 100))
+y = np.array([1, 2, 3, 4, 5, 6])
+from sklearn.naive_bayes import BernoulliNB
+clf = BernoulliNB()
+clf.fit(X, y)
+print(clf.predict(X[2:3]))
