9b4febe58d1b921ca24298b8f6d475b5b0e35a75
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-09-22 22:43:48 +0200

    vector: add copyright note

--- poo/vector/app/vector.py
+++ poo/vector/app/vector.py
@@ -1,4 +1,25 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
 
 class Vector:
    def __init__(self, a, b):
@@ -7,7 +28,6 @@ class Vector:
 
    def __str__(self):
       return 'Vector (%d, %d)' % (self.a, self.b)
-   
+
    def __add__(self,other):
       return Vector(self.a + other.a, self.b + other.b)
-
