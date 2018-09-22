049af709bb54be437084e6ed3769f6315e1983ca
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-09-22 19:03:35 +0200

    basics: add copyright note

--- basics/factors.py
+++ basics/factors.py
@@ -1,4 +1,26 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+#!/usr/bin/python
 
 def factors(b):
     for i in range(1, b+1):
@@ -6,7 +28,7 @@ def factors(b):
             print(i)
 
 if __name__ == '__main__':
-    b = input('Your Number Please:')
+    b = input('Your Number Please: ')
     b = float(b)
 
     if (b > 0) and b.is_integer():
