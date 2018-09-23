6dd4af3eec606b63de496609cbae41f61b257ad9
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-09-23 09:33:02 +0200

    pandas: add copyright note

--- pandas/pandas-10min.py
+++ pandas/pandas-10min.py
@@ -1,3 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
 import pandas as pd
 import numpy as np
 #import matplotlib.pyplot as plt
@@ -332,4 +354,3 @@ print pd.read_csv('foo.csv')
 # df.to_excel('foo.xlsx', sheet_name='Sheet1')
 
 # pd.read_excel('foo.xlsx', 'Sheet1', index_col=None, na_values=['NA'])
-
