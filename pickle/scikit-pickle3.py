5b1ecf01c0d2b517bf80b3380b82001c094b922f
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-09-23 09:28:05 +0200

    pickle: add copyright note

--- pickle/scikit-pickle3.py
+++ pickle/scikit-pickle3.py
@@ -1,6 +1,28 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from sklearn import svm
 from sklearn import datasets
 clf = svm.SVC()
@@ -13,4 +35,3 @@ s = pickle.dumps(clf)
 clf2 = pickle.loads(s)
 clf2.predict(X[0:1])
 print(clf2.fit(X,y))
-
