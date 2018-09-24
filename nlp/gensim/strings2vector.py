310bfef0061f99faee52c7cf551d8a8855dafa06
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-09-24 19:41:36 +0200

    strings2vector.py: creation

---
+++ nlp/gensim/strings2vector.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from gensim import corpora
+
+documents = ["Human machine interface for lab abc computer applications",
+             "A survey of user opinion of computer system response time",
+             "The EPS user interface management system",
+             "System and human system engineering testing of EPS",
+             "Relation of user perceived response time to error measurement",
+             "The generation of random binary unordered trees",
+             "The intersection graph of paths in trees",
+             "Graph minors IV Widths of trees and well quasi ordering",
+             "Graph minors A survey"]
+
+# dictionary = corpora.Dictionary.load('/tmp/deerwester.dict')
+# corpus = corpora.MmCorpus('/tmp/deerwester.mm') # comes from the first tutorial, "From strings to vectors"
+# print(corpus)
+
+# remove common words and tokenize
+stoplist = set('for a of the and to in'.split())
+texts = [[word for word in document.lower().split() if word not in stoplist]
+         for document in documents]
+
+# remove words that appear only once
+from collections import defaultdict
+frequency = defaultdict(int)
+for text in texts:
+    for token in text:
+        frequency[token] += 1
+
+texts = [[token for token in text if frequency[token] > 1]
+         for text in texts]
+
+from pprint import pprint  # pretty-printer
+pprint(texts)
+
