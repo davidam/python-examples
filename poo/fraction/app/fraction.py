1ecf0b1f98c755e389b26826d0310d7d270e516b
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-09-24 21:27:11 +0200

    fraction: add copyright note

--- poo/fraction/app/fraction.py
+++ poo/fraction/app/fraction.py
@@ -1,10 +1,31 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
 
 class Fraction(object):
 
     def __init__(self, n, d):
         self.numerator, self.denominator = self.reduce(n, d)
-        
+
 
     @staticmethod
     def gcd(a,b):
@@ -19,5 +40,3 @@ class Fraction(object):
 
     def __str__(self):
         return str(self.numerator)+'/'+str(self.denominator)
-
-
