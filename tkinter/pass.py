9d1a1bc2a15dcfdaf5bc7bc7279aef52aa16e7f8
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-09-25 15:18:17 +0200

    tkinter: add copyright note

--- tkinter/pass.py
+++ tkinter/pass.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
 
 # Copyright (C) 2018  David Arroyo Menéndez
 
@@ -18,7 +16,7 @@
 
 # You should have received a copy of the GNU General Public License
 # along with GNU Emacs; see the file COPYING.  If not, write to
-# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
 
 from tkinter import *
@@ -31,36 +29,36 @@ class Aplicacion():
     def __init__(self):
         self.raiz = Tk()
         self.raiz.title("Acceso")
-        
+
         # Cambia el formato de la fuente actual a negrita para
         # resaltar las dos etiquetas que acompañan a las cajas
-        # de entrada. (Para este cambio se ha importado el  
+        # de entrada. (Para este cambio se ha importado el
         # módulo 'font' al comienzo del programa):
-        
+
         fuente = font.Font(weight='bold')
-        
+
         # Define las etiquetas que acompañan a las cajas de
-        # entrada y asigna el formato de fuente anterior: 
-                               
-        self.etiq1 = ttk.Label(self.raiz, text="Usuario:", 
+        # entrada y asigna el formato de fuente anterior:
+
+        self.etiq1 = ttk.Label(self.raiz, text="Usuario:",
                                font=fuente)
-        self.etiq2 = ttk.Label(self.raiz, text="Contraseña:", 
+        self.etiq2 = ttk.Label(self.raiz, text="Contraseña:",
                                font=fuente)
-        
+
         # Declara dos variables de tipo cadena para contener
-        # el usuario y la contraseña: 
-        
+        # el usuario y la contraseña:
+
         self.usuario = StringVar()
         self.clave = StringVar()
-        
-        # Realiza una lectura del nombre de usuario que 
+
+        # Realiza una lectura del nombre de usuario que
         # inició sesión en el sistema y lo asigna a la
         # variable 'self.usuario' (Para capturar esta
         # información se ha importando el módulo getpass
         # al comienzo del programa):
-        
+
         self.usuario.set(getpass.getuser())
-        
+
         # Define dos cajas de entrada que aceptarán cadenas
         # de una longitud máxima de 30 caracteres.
         # A la primera de ellas 'self.ctext1' que contendrá
@@ -70,81 +68,81 @@ class Aplicacion():
         # programa quedará reflejada en la caja de entrada.
         # En la segunda caja de entrada, la de la contraseña,
         # se hace lo mismo. Además, se establece la opción
-        # 'show' con un "*" (asterisco) para ocultar la 
+        # 'show' con un "*" (asterisco) para ocultar la
         # escritura de las contraseñas:
-        
-        self.ctext1 = ttk.Entry(self.raiz, 
-                                textvariable=self.usuario, 
+
+        self.ctext1 = ttk.Entry(self.raiz,
+                                textvariable=self.usuario,
                                 width=30)
-        self.ctext2 = ttk.Entry(self.raiz, 
-                                textvariable=self.clave, 
+        self.ctext2 = ttk.Entry(self.raiz,
+                                textvariable=self.clave,
                                 width=30, show="*")
         self.separ1 = ttk.Separator(self.raiz, orient=HORIZONTAL)
-        
+
         # Se definen dos botones con dos métodos: El botón
         # 'Aceptar' llamará al método 'self.aceptar' cuando
         # sea presionado para validar la contraseña; y el botón
         # 'Cancelar' finalizará la aplicación si se llega a
         # presionar:
-        
-        self.boton1 = ttk.Button(self.raiz, text="Aceptar", 
+
+        self.boton1 = ttk.Button(self.raiz, text="Aceptar",
                                  command=self.aceptar)
-        self.boton2 = ttk.Button(self.raiz, text="Cancelar", 
+        self.boton2 = ttk.Button(self.raiz, text="Cancelar",
                                  command=quit)
-                                 
+
         # Se definen las posiciones de los widgets dentro de
-        # la ventana. Todos los controles se van colocando 
-        # hacia el lado de arriba, excepto, los dos últimos, 
+        # la ventana. Todos los controles se van colocando
+        # hacia el lado de arriba, excepto, los dos últimos,
         # los botones, que se situarán debajo del último 'TOP':
         # el primer botón hacia el lado de la izquierda y el
         # segundo a su derecha.
-        # Los valores posibles para la opción 'side' son: 
+        # Los valores posibles para la opción 'side' son:
         # TOP (arriba), BOTTOM (abajo), LEFT (izquierda)
         # y RIGHT (derecha). Si se omite, el valor será TOP
         # La opción 'fill' se utiliza para indicar al gestor
         # cómo expandir/reducir el widget si la ventana cambia
         # de tamaño. Tiene tres posibles valores: BOTH
-        # (Horizontal y Verticalmente), X (Horizontalmente) e 
+        # (Horizontal y Verticalmente), X (Horizontalmente) e
         # Y (Verticalmente). Funcionará si el valor de la opción
         # 'expand' es True.
         # Por último, las opciones 'padx' y 'pady' se utilizan
-        # para añadir espacio extra externo horizontal y/o 
-        # vertical a los widgets para separarlos entre sí y de 
+        # para añadir espacio extra externo horizontal y/o
+        # vertical a los widgets para separarlos entre sí y de
         # los bordes de la ventana. Hay otras equivalentes que
         # añaden espacio extra interno: 'ipàdx' y 'ipady':
-                                         
-        self.etiq1.pack(side=TOP, fill=BOTH, expand=True, 
+
+        self.etiq1.pack(side=TOP, fill=BOTH, expand=True,
                         padx=5, pady=5)
-        self.ctext1.pack(side=TOP, fill=X, expand=True, 
+        self.ctext1.pack(side=TOP, fill=X, expand=True,
                          padx=5, pady=5)
-        self.etiq2.pack(side=TOP, fill=BOTH, expand=True, 
+        self.etiq2.pack(side=TOP, fill=BOTH, expand=True,
                         padx=5, pady=5)
-        self.ctext2.pack(side=TOP, fill=X, expand=True, 
+        self.ctext2.pack(side=TOP, fill=X, expand=True,
                          padx=5, pady=5)
-        self.separ1.pack(side=TOP, fill=BOTH, expand=True, 
+        self.separ1.pack(side=TOP, fill=BOTH, expand=True,
                          padx=5, pady=5)
-        self.boton1.pack(side=LEFT, fill=BOTH, expand=True, 
+        self.boton1.pack(side=LEFT, fill=BOTH, expand=True,
                          padx=5, pady=5)
-        self.boton2.pack(side=RIGHT, fill=BOTH, expand=True, 
+        self.boton2.pack(side=RIGHT, fill=BOTH, expand=True,
                          padx=5, pady=5)
-        
+
         # Cuando se inicia el programa se asigna el foco
         # a la caja de entrada de la contraseña para que se
         # pueda empezar a escribir directamente:
-                
+
         self.ctext2.focus_set()
-        
+
         self.raiz.mainloop()
-    
-    # El método 'aceptar' se emplea para validar la 
-    # contraseña introducida. Será llamado cuando se 
+
+    # El método 'aceptar' se emplea para validar la
+    # contraseña introducida. Será llamado cuando se
     # presione el botón 'Aceptar'. Si la contraseña
     # coincide con la cadena 'tkinter' se imprimirá
-    # el mensaje 'Acceso permitido' y los valores 
+    # el mensaje 'Acceso permitido' y los valores
     # aceptados. En caso contrario, se mostrará el
     # mensaje 'Acceso denegado' y el foco volverá al
     # mismo lugar.
-    
+
     def aceptar(self):
         if self.clave.get() == 'tkinter':
             print("Acceso permitido")
@@ -152,13 +150,13 @@ class Aplicacion():
             print("Contraseña:", self.ctext2.get())
         else:
             print("Acceso denegado")
-            
+
             # Se inicializa la variable 'self.clave' para
             # que el widget 'self.ctext2' quede limpio.
             # Por último, se vuelve a asignar el foco
             # a este widget para poder escribir una nueva
             # contraseña.
-            
+
             self.clave.set("")
             self.ctext2.focus_set()
 
