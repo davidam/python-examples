f13263898e0d6b7fe56db05302a031c80b171e41
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-09-26 09:42:00 +0200

    table.py: to PyQT5

--- qt/table.py
+++ qt/table.py
@@ -1,31 +1,81 @@
-from PyQt4.QtGui import * 
-from PyQt4.QtCore import * 
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
 import sys
- 
-def main():  
-    app 	= QApplication(sys.argv)
-    table 	= QTableWidget()
-    tableItem 	= QTableWidgetItem()
- 
-    # initiate table
-    table.setWindowTitle("QTableWidget Example @pythonspot.com")
-    table.resize(400, 250)
-    table.setRowCount(4)
-    table.setColumnCount(2)
- 
-    # set data
-    table.setItem(0,0, QTableWidgetItem("Item (1,1)"))
-    table.setItem(0,1, QTableWidgetItem("Item (1,2)"))
-    table.setItem(1,0, QTableWidgetItem("Item (2,1)"))
-    table.setItem(1,1, QTableWidgetItem("Item (2,2)"))
-    table.setItem(2,0, QTableWidgetItem("Item (3,1)"))
-    table.setItem(2,1, QTableWidgetItem("Item (3,2)"))
-    table.setItem(3,0, QTableWidgetItem("Item (4,1)"))
-    table.setItem(3,1, QTableWidgetItem("Item (4,2)"))
- 
-    # show table
-    table.show()
-    return app.exec_()
- 
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QAction, QTableWidget,QTableWidgetItem,QVBoxLayout
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+
+class App(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 table - pythonspot.com'
+        self.left = 0
+        self.top = 0
+        self.width = 300
+        self.height = 200
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.createTable()
+
+        # Add box layout, add table to box layout and add box layout to widget
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.tableWidget)
+        self.setLayout(self.layout)
+
+        # Show widget
+        self.show()
+
+    def createTable(self):
+       # Create table
+        self.tableWidget = QTableWidget()
+        self.tableWidget.setRowCount(4)
+        self.tableWidget.setColumnCount(2)
+        self.tableWidget.setItem(0,0, QTableWidgetItem("Cell (1,1)"))
+        self.tableWidget.setItem(0,1, QTableWidgetItem("Cell (1,2)"))
+        self.tableWidget.setItem(1,0, QTableWidgetItem("Cell (2,1)"))
+        self.tableWidget.setItem(1,1, QTableWidgetItem("Cell (2,2)"))
+        self.tableWidget.setItem(2,0, QTableWidgetItem("Cell (3,1)"))
+        self.tableWidget.setItem(2,1, QTableWidgetItem("Cell (3,2)"))
+        self.tableWidget.setItem(3,0, QTableWidgetItem("Cell (4,1)"))
+        self.tableWidget.setItem(3,1, QTableWidgetItem("Cell (4,2)"))
+        self.tableWidget.move(0,0)
+
+        # table selection change
+        self.tableWidget.doubleClicked.connect(self.on_click)
+
+    @pyqtSlot()
+    def on_click(self):
+        print("\n")
+        for currentQTableWidgetItem in self.tableWidget.selectedItems():
+            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
+
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
