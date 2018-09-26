7a85d5b0a7d6bf6e7228dd6adb19ae7f57bf36fa
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-09-26 09:44:46 +0200

    tabs.py: to PyQT5

--- qt/tabs.py
+++ qt/tabs.py
@@ -1,43 +1,81 @@
-from PyQt4 import QtGui
-from PyQt4 import QtCore
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
 import sys
- 
-def main():
- 
-    app 	= QtGui.QApplication(sys.argv)
-    tabs	= QtGui.QTabWidget()
- 
-    # Create tabs
-    tab1	= QtGui.QWidget()	
-    tab2	= QtGui.QWidget()
-    tab3	= QtGui.QWidget()
-    tab4	= QtGui.QWidget()
- 
-    # Resize width and height
-    tabs.resize(250, 150)
- 
-    # Set layout of first tab
-    vBoxlayout	= QtGui.QVBoxLayout()
-    pushButton1 = QtGui.QPushButton("Start")
-    pushButton2 = QtGui.QPushButton("Settings")
-    pushButton3 = QtGui.QPushButton("Stop")
-    vBoxlayout.addWidget(pushButton1)
-    vBoxlayout.addWidget(pushButton2)
-    vBoxlayout.addWidget(pushButton3)
-    tab1.setLayout(vBoxlayout)   
- 
-    # Add tabs
-    tabs.addTab(tab1,"Tab 1")
-    tabs.addTab(tab2,"Tab 2")
-    tabs.addTab(tab3,"Tab 3")
-    tabs.addTab(tab4,"Tab 4") 
- 
-    # Set title and show
-    tabs.setWindowTitle('PyQt QTabWidget @ pythonspot.com')
-    tabs.show()
- 
-    sys.exit(app.exec_())
- 
- 
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QWidget, QAction, QTabWidget,QVBoxLayout
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+
+class App(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 tabs - pythonspot.com'
+        self.left = 0
+        self.top = 0
+        self.width = 300
+        self.height = 200
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        self.table_widget = MyTableWidget(self)
+        self.setCentralWidget(self.table_widget)
+
+        self.show()
+
+class MyTableWidget(QWidget):
+
+    def __init__(self, parent):
+        super(QWidget, self).__init__(parent)
+        self.layout = QVBoxLayout(self)
+
+        # Initialize tab screen
+        self.tabs = QTabWidget()
+        self.tab1 = QWidget()
+        self.tab2 = QWidget()
+        self.tabs.resize(300,200)
+
+        # Add tabs
+        self.tabs.addTab(self.tab1,"Tab 1")
+        self.tabs.addTab(self.tab2,"Tab 2")
+
+        # Create first tab
+        self.tab1.layout = QVBoxLayout(self)
+        self.pushButton1 = QPushButton("PyQt5 button")
+        self.tab1.layout.addWidget(self.pushButton1)
+        self.tab1.setLayout(self.tab1.layout)
+
+        # Add tabs to widget
+        self.layout.addWidget(self.tabs)
+        self.setLayout(self.layout)
+
+    @pyqtSlot()
+    def on_click(self):
+        print("\n")
+        for currentQTableWidgetItem in self.tableWidget.selectedItems():
+            print(currentQTableWidgetItem.row(), currentQTableWidgetItem.column(), currentQTableWidgetItem.text())
+
 if __name__ == '__main__':
-    main()
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
