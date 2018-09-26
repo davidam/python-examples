360fd500f3881dc23e585ffd2c608207aa1c5877
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-09-26 12:39:08 +0200

    window.py: to pyqt5

--- qt/window.py
+++ qt/window.py
@@ -1,22 +1,47 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
 import sys
-from PyQt4.QtGui import *
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
 
-# Create an PyQT4 application object.
-a = QApplication(sys.argv)
+class App(QWidget):
 
-# The QWidget widget is the base class of all user interface objects in PyQt4.
-w = QWidget()
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 simple window - davidam.com'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+        self.initUI()
 
-# Set window size.
-w.resize(320, 240)
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.show()
 
-# Set window title
-w.setWindowTitle("Hello World!")
-
-# Show window
-w.show()
-
-sys.exit(a.exec_())
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
