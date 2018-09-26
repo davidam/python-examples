543e8f0b236c1b5689bb5c089ff7f907c6a9c159
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-09-26 12:48:15 +0200

    textboxwidget.py: to PyQt5

--- qt/textboxwidget.py
+++ qt/textboxwidget.py
@@ -1,27 +1,67 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
 import sys
-from PyQt4.QtGui import *
- 
-# Create an PyQT4 application object.
-a = QApplication(sys.argv)       
- 
-# The QWidget widget is the base class of all user interface objects in PyQt4.
-w = QMainWindow()
- 
-# Set window size. 
-w.resize(320, 100)
- 
-# Set window title  
-w.setWindowTitle("PyQT Python Widget!") 
- 
-# Create textbox
-textbox = QLineEdit(w)
-textbox.move(20, 20)
-textbox.resize(280,40)
- 
-# Show window
-w.show() 
- 
-sys.exit(a.exec_())
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction, QLineEdit, QMessageBox
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+
+class App(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 textbox - davidam.com'
+        self.left = 10
+        self.top = 10
+        self.width = 400
+        self.height = 140
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        # Create textbox
+        self.textbox = QLineEdit(self)
+        self.textbox.move(20, 20)
+        self.textbox.resize(280,40)
+
+        # Create a button in the window
+        self.button = QPushButton('Show text', self)
+        self.button.move(20,80)
+
+        # connect button to function on_click
+        self.button.clicked.connect(self.on_click)
+        self.show()
+
+    @pyqtSlot()
+    def on_click(self):
+        textboxValue = self.textbox.text()
+        QMessageBox.question(self, 'Message - davidam.com', "You typed: " + textboxValue, QMessageBox.Ok, QMessageBox.Ok)
+        self.textbox.setText("")
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
