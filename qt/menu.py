ea3be085b4f68b4080bdb517420508735e07d778
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-09-26 09:33:46 +0200

    menu.py: creation

--- qt/menu.py
+++ qt/menu.py
@@ -1,36 +1,64 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 #
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
 import sys
-from PyQt4.QtGui import *
- 
-# Create an PyQT4 application object.
-a = QApplication(sys.argv)       
- 
-# The QWidget widget is the base class of all user interface objects in PyQt4.
-w = QMainWindow()
- 
-# Set window size. 
-w.resize(320, 240)
- 
-# Set window title  
-w.setWindowTitle("Hello World!") 
- 
-# Create main menu
-mainMenu = w.menuBar()
-mainMenu.setNativeMenuBar(False)
-fileMenu = mainMenu.addMenu('File')
- 
-# Add exit button
-#exitButton = QAction(QIcon('exit24.png'), 'Exit', w)
-exitButton = QAction('Exit', w)
-exitButton.setShortcut('Ctrl+Q')
-exitButton.setStatusTip('Exit application')
-exitButton.triggered.connect(w.close)
-fileMenu.addAction(exitButton)
- 
- 
-# Show window
-w.show() 
- 
-sys.exit(a.exec_())
+from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget, QPushButton, QAction
+from PyQt5.QtGui import QIcon
+from PyQt5.QtCore import pyqtSlot
+
+class App(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 menu - pythonspot.com'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 400
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+
+        mainMenu = self.menuBar()
+        fileMenu = mainMenu.addMenu('File')
+        editMenu = mainMenu.addMenu('Edit')
+        viewMenu = mainMenu.addMenu('View')
+        searchMenu = mainMenu.addMenu('Search')
+        toolsMenu = mainMenu.addMenu('Tools')
+        helpMenu = mainMenu.addMenu('Help')
+
+        exitButton = QAction(QIcon('exit24.png'), 'Exit', self)
+        exitButton.setShortcut('Ctrl+Q')
+        exitButton.setStatusTip('Exit application')
+        exitButton.triggered.connect(self.close)
+        fileMenu.addAction(exitButton)
+
+        self.show()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
