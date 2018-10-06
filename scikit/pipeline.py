7d7e2e79aeaf4d1437a437294ed0754b240b9913
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-10-06 13:42:25 +0200

    pipeline.py: creation

---
+++ scikit/pipeline.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+# Es posible hacer pipes dese un algoritmo a otro usando esta librería
+
+from sklearn import svm
+from sklearn.datasets import samples_generator
+from sklearn.feature_selection import SelectKBest
+from sklearn.feature_selection import f_regression
+from sklearn.pipeline import Pipeline
+# generate some data to play with
+X, y = samples_generator.make_classification(
+    n_informative=5, n_redundant=0, random_state=42)
+# ANOVA SVM-C
+anova_filter = SelectKBest(f_regression, k=5)
+clf = svm.SVC(kernel='linear')
+anova_svm = Pipeline([('anova', anova_filter), ('svc', clf)])
+# You can set the parameters using the names issued
+# For instance, fit using a k of 10 in the SelectKBest
+# and a parameter 'C' of the svm
+anova_svm.set_params(anova__k=10, svc__C=.1).fit(X, y)
+
+prediction = anova_svm.predict(X)
+print(anova_svm.score(X, y))
+
+# getting the selected features chosen by anova_filter
+anova_svm.named_steps['anova'].get_support()
+
+# Another way to get selected features chosen by anova_filter
+print(anova_svm.named_steps.anova.get_support())
