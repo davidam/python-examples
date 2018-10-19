75caa99a5ce5179d19a1b0b56e12b347a4aa6227
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-10-19 16:02:57 +0200

    scikit/pca.py: creation

---
+++ scikit/pca.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+## LOAD IRIS DATASET
+from pprint import pprint
+import pandas as pd
+import matplotlib.pyplot as plt
+
+url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+# load dataset into Pandas DataFrame
+df = pd.read_csv(url, names=['sepal length','sepal width','petal length','petal width','target'])
+print("STEP1: 4 COMPONENTS + 1 TARGET")
+pprint(df)
+from sklearn.preprocessing import StandardScaler
+features = ['sepal length', 'sepal width', 'petal length', 'petal width']
+# Separating out the features
+x = df.loc[:, features].values
+# Separating out the target
+y = df.loc[:,['target']].values
+# Standardizing the features
+x = StandardScaler().fit_transform(x)
+
+## STANDARIZE THE DATA
+
+from sklearn.preprocessing import StandardScaler
+features = ['sepal length', 'sepal width', 'petal length', 'petal width']
+# Separating out the features
+x = df.loc[:, features].values
+#print(list(.columns.values))
+# Separating out the target
+y = df.loc[:,['target']].values
+# Standardizing the features
+x = StandardScaler().fit_transform(x)
+print("STEP2: STANDARIZE THE DATA")
+pprint(x)
+
+## PCA PROJECTION TO 2D
+
+from sklearn.decomposition import PCA
+pca = PCA(n_components=2)
+principalComponents = pca.fit_transform(x)
+print("STEP3: PCA PROJECTION TO 2D")
+pprint(principalComponents)
+principalDf = pd.DataFrame(data = principalComponents, columns = ['principal component 1', 'principal component 2'])
+pprint(principalDf)
+print(list(principalDf.columns.values))
+finalDf = pd.concat([principalDf, df[['target']]], axis = 1)
+
+## Visualize 2D Projection
+
+fig = plt.figure(figsize = (8,8))
+ax = fig.add_subplot(1,1,1)
+ax.set_xlabel('Principal Component 1', fontsize = 15)
+ax.set_ylabel('Principal Component 2', fontsize = 15)
+ax.set_title('2 component PCA', fontsize = 20)
+targets = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+colors = ['r', 'g', 'b']
+for target, color in zip(targets,colors):
+    indicesToKeep = finalDf['target'] == target
+    ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
+               , finalDf.loc[indicesToKeep, 'principal component 2']
+               , c = color
+               , s = 50)
+ax.legend(targets)
+ax.grid()
+
+#plt.show()
