69428119f468e068829e963d6103d7959066ee13
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-11-29 17:31:15 +0100

    list2file.py: another way with open and write

--- basics/list2file.py
+++ basics/list2file.py
@@ -1,5 +1,17 @@
 
 import simplejson
 f = open('/tmp/output.txt', 'w')
-simplejson.dump([[1, 2, 3, 4],2,3,4], f)
+l1 = [[1, 2, 3, 4],2,3,4]
+l2 = [1, 2, 3, 4]
+simplejson.dump(l1, f)
 f.close()
+
+i = 0
+line = ""
+while (i < (len(l2) -1)):
+    line = line + str(l2[i]) + ", "
+    i = i +1
+line = line + str(l2[i])
+f2 = open('/tmp/output2.txt', 'w')
+f2.write(line)
+f2.close()
