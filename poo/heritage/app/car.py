b43a99ad73f33ccac5504eb4551f61ece7dbdd19
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-12-09 19:34:20 +0100

    app/car.py: fix bug

--- poo/heritage/app/car.py
+++ poo/heritage/app/car.py
@@ -21,7 +21,7 @@
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
 
-from app.vehicle import Vehicle
+from vehicle import Vehicle
 
 class Car(Vehicle):
 
