5286e539a3c6b3c3a58e98a9d3aefb463a17ffd6
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2018-12-18 09:05:46 +0100

    pickle-load.py: more models

--- pickle/pickle-load.py
+++ pickle/pickle-load.py
@@ -30,4 +30,22 @@ pprint.pprint(data1)
 data2 = pickle.load(pkl_file)
 pprint.pprint(data2)
 
+pkl_file2 = open('finalized_model.sav', 'rb')
+data3 = pickle.load(pkl_file2)
+pprint.pprint(data3)
+
+pkl_file3 = open('bernoulliNB_model.sav', 'rb')
+data4 = pickle.load(pkl_file3)
+pprint.pprint(data4)
+
+pkl_file4 = open('gaussianNB_model.sav', 'rb')
+data5 = pickle.load(pkl_file4)
+pprint.pprint(data5)
+
+pkl_file5 = open('multinomialNB_model.sav', 'rb')
+data6 = pickle.load(pkl_file5)
+pprint.pprint(data6)
+
+
 pkl_file.close()
+pkl_file2.close()
