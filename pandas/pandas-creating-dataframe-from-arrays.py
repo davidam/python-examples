2dccd2e269c7fdbfd32a7ad77d79cb9b624e897e
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-01-25 15:20:09 +0100

    pandas-creating-dataframe-from-arrays.py: to python3

--- pandas/pandas-creating-dataframe-from-arrays.py
+++ pandas/pandas-creating-dataframe-from-arrays.py
@@ -26,4 +26,4 @@ t = np.linspace(-6, 6, 20)
 sin_t = np.sin(t)
 cos_t = np.cos(t)
 
-print pandas.DataFrame({'t': t, 'sin': sin_t, 'cos': cos_t})
+print(pandas.DataFrame({'t': t, 'sin': sin_t, 'cos': cos_t}))
