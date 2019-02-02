e11d41fcfe1436dc33cf37bcb0f5a80f548bffda
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-02-02 21:09:21 +0100

    write.py: to python3

--- basics/write.py
+++ basics/write.py
@@ -23,8 +23,8 @@
 #!/usr/bin/python
 
 # Abrimos el archivo fichero.txt
-fo = open("/tmp/fichero.txt", "wb")
-fo.write( "Gora python\n");
+fo = open("/tmp/fichero.txt", "w")
+fo.write("Gora python\n");
 
 # Cerramos el archivo fichero.txt
 fo.close()
