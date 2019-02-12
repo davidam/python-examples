a1b5afdbbeba571eaf0e6604709193247c0898f8
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-02-12 19:53:34 +0100

    pca-example.py: minimal changes

--- scikit/pca-example.py
+++ scikit/pca-example.py
@@ -3,19 +3,18 @@
 
 import numpy as np
 from sklearn.decomposition import PCA
-X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-pca = PCA(n_components=2)
+X = np.array([[-1, -1, 1, 2], [-2, -1, -1, 1], [-3, -2, 2, 0], [1, 1, 2, 3], [2, 1, 2, 4], [3, 2, 2, 0]])
+pca = PCA(n_components=4)
 pca.fit(X)
-print(pca.explained_variance_ratio_)  
-print(pca.singular_values_)  
+print(pca.explained_variance_ratio_)
+print(pca.singular_values_)
 
 pca = PCA(n_components=2, svd_solver='full')
-pca.fit(X)                 
-print(pca.explained_variance_ratio_)  
-print(pca.singular_values_)  
+pca.fit(X)
+print(pca.explained_variance_ratio_)
+print(pca.singular_values_)
 
 pca = PCA(n_components=1, svd_solver='arpack')
 pca.fit(X)
-print(pca.explained_variance_ratio_)  
-print(pca.singular_values_)  
-
+print(pca.explained_variance_ratio_)
+print(pca.singular_values_)
