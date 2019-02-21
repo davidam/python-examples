baea63505f79357c334b55e0980131aa9c88c7d7
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-02-21 15:19:24 +0100

    pca-nice.py: creation

---
+++ scikit/pca-nice.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import pandas as pd
+import pylab as pl
+from sklearn import datasets
+from sklearn.decomposition import PCA
+
+# load dataset
+iris = datasets.load_iris()
+df = pd.DataFrame(iris.data, columns=iris.feature_names)
+
+# normalize data
+from sklearn import preprocessing
+data_scaled = pd.DataFrame(preprocessing.scale(df),columns = df.columns)
+
+# PCA
+pca = PCA(n_components=2)
+pca.fit_transform(data_scaled)
+
+# Dump components relations with features:
+print(pd.DataFrame(pca.components_,columns=data_scaled.columns,index = ['PC-1','PC-2']))
