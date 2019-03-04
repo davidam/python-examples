67684e272b58c4cddd7fdf602472f8a2c0a3a49b
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-03-04 09:28:30 +0100

    delete-doubles-csv.py: creation

---
+++ shell/delete-doubles-csv.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import csv
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('--csv', default="files/kernel-authors.csv")
+args = parser.parse_args()
+
+
+with open(args.csv,'r') as in_file, open('2.csv','w') as out_file:
+    seen = set() # set for fast O(1) amortized lookup
+    for line in in_file:
+        if line in seen: continue # skip duplicate
+
+        seen.add(line)
+        out_file.write(line)
