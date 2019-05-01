0153c1e785d015dcd53faaf269aeb85c0fcab987
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-05-02 00:18:30 +0200

    basics/file-lines.py: add match

--- basics/file-lines.py
+++ basics/file-lines.py
@@ -20,9 +20,19 @@
 # along with GNU Emacs; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
+import re
+
 filepath="README.org"
 with open(filepath) as fp:
     for cnt, line in enumerate(fp):
         print("Line {}: {}".format(cnt, line)) # Imprime línea y número de línea
         print(line[0])         # Imprime el primer caracter
         print(line[(len(line) -2)]) # Imprime el último carácter
+        match = re.search(r'flask', line)
+        # If-statement after search() tests if it succeeded
+        if match:
+            flaskcnt = cnt
+            flaskline = line
+
+print("---------------------------------------------------------------------------")
+print("Flask is in the line {}. The full text is: {}".format(flaskcnt, flaskline)) # match.group() ## 'found word:cat'
