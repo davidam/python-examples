288eeba699b60a878c1a5e1942709b781f4f1af7
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-05-01 17:36:43 +0200

    basics/file-lines.py: add print first character

--- basics/file-lines.py
+++ basics/file-lines.py
@@ -23,4 +23,5 @@
 filepath="README.org"
 with open(filepath) as fp:
     for cnt, line in enumerate(fp):
-        print("Line {}: {}".format(cnt, line))
+        print("Line {}: {}".format(cnt, line)) # Imprime línea y número de línea
+        print(line[0])         # Imprime caracter primero
