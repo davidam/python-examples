d7b971256440261bb7fdb3b970036992c479ce0b
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-05-03 20:00:24 +0200

    scipy/scipy-poisson.py: some prints

--- scipy/scipy-poisson.py
+++ scipy/scipy-poisson.py
@@ -5,9 +5,11 @@ fig, ax = plt.subplots(1, 1)
 
 mu = 0.6
 mean, var, skew, kurt = poisson.stats(mu, moments='mvsk')
+print("mean: %s, var: %s, skew: %s, kurt: %s" % (mean, var, skew, kurt))
 
 x = np.arange(poisson.ppf(0.01, mu),
               poisson.ppf(0.99, mu))
+print(x)
 ax.plot(x, poisson.pmf(x, mu), 'bo', ms=8, label='poisson pmf')
 ax.vlines(x, 0, poisson.pmf(x, mu), colors='b', lw=5, alpha=0.5)
 
@@ -15,9 +17,13 @@ rv = poisson(mu)
 ax.vlines(x, 0, rv.pmf(x), colors='k', linestyles='-', lw=1,
         label='frozen pmf')
 ax.legend(loc='best', frameon=False)
-plt.show()
-
+#plt.show()
+print(np.array([0.54881164, 0.87809862, 0.97688471]))
 prob = poisson.cdf(x, mu)
-np.allclose(x, poisson.ppf(prob, mu))
+print(prob)
+b = np.array_equal(prob, np.array([0.54881164, 0.87809862, 0.97688471]))
+print("Boolean: %s" % b)
+print(np.allclose(x, poisson.ppf(prob, mu)))
 
 r = poisson.rvs(mu, size=1000)
+print(r)
