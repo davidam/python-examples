bfc89f848cc7f871429be7a4720802b184cf30c3
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-05-14 18:03:42 +0200

    algorithms/bfs2.py: creation

---
+++ algorithms/bfs2.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+# sample graph implemented as a dictionary
+graph = {'A': ['B', 'C', 'E'],
+         'B': ['A','D', 'E'],
+         'C': ['A', 'F', 'G'],
+         'D': ['B'],
+         'E': ['A', 'B','D'],
+         'F': ['C'],
+         'G': ['C']}
+
+
+# visits all the nodes of a graph (connected component) using BFS
+def bfs_connected_component(graph, start):
+    # keep track of all visited nodes
+    explored = []
+    # keep track of nodes to be checked
+    queue = [start]
+
+    levels = {}         # this dict keeps track of levels
+    levels[start]= 0    # depth of start node is 0
+
+    visited= [start]     # to avoid inserting the same node twice into the queue
+
+    # keep looping until there are nodes still to be checked
+    while queue:
+       # pop shallowest node (first node) from queue
+        node = queue.pop(0)
+        explored.append(node)
+        neighbours = graph[node]
+
+        # add neighbours of node to queue
+        for neighbour in neighbours:
+            if neighbour not in visited:
+                queue.append(neighbour)
+                visited.append(neighbour)
+
+                levels[neighbour]= levels[node]+1
+                # print(neighbour, ">>", levels[neighbour])
+
+    print(levels)
+
+    return explored
+
+ans = bfs_connected_component(graph,'A') # returns ['A', 'B', 'C', 'E', 'D', 'F', 'G']
+print(ans)
