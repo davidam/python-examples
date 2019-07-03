18f754243bbbbc076e951729322af6f40f2e5017
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-07-03 11:52:27 +0200

    basics/json/json2file.py: creation

---
+++ basics/json/json2file.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with python-examples; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import json
+
+jsonvar = json.dumps(['foo', {'bar': ('baz', None, 1.0, 2)}])
+fo = open("/tmp/fichero.txt", "w")
+fo.write(jsonvar)
+# Cerramos el archivo fichero.txt
+fo.close()
