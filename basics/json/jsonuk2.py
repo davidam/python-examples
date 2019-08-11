8e36299d9e82aae40349d567ecf85078a5335f82
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-08-11 16:12:15 +0200

    jsonuk2.py: creation

---
+++ basics/json/jsonuk2.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import pandas as pd
+from pandas.io.json import json_normalize
+
+import json
+from pprint import pprint
+
+jsondata = open('jsonuk2.json').read()
+json_object = json.loads(jsondata)
+pprint(json_object)
+print(json_object["Anna"])
+print(json_object["Anna"].keys())
+print(json_object["Anna"].values())
+print(json_object["Anna"]['gender'])
+print(json_object["Anna"]['count'])
+
+for i in json_object:
+    print(i)
