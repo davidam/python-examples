d1202392a1fc5bb523af75c4efba754babe179a9
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-09-14 00:34:24 +0200

    sparql/scientists-by-gender.py: creation

---
+++ sparql/scientists-by-gender.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with wikidata-example; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import requests
+
+url = 'https://query.wikidata.org/sparql'
+query = """
+SELECT ?gender (count(distinct ?human) as ?number)
+WHERE
+{
+	?human wdt:P31 wd:Q5
+	; wdt:P21 ?gender
+	; wdt:P106/wdt:P279* wd:Q901 .
+}
+GROUP BY ?gender
+LIMIT 10
+"""
+r = requests.get(url, params = {'format': 'json', 'query': query})
+data = r.json()
+print(data)
