6413adbb455797e7f4ee2b6ba45ee99e3be1c16f
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-09-26 10:28:08 +0200

    scikit/svr-example.py: fix example

--- scikit/svr-example.py
+++ scikit/svr-example.py
@@ -3,9 +3,10 @@
 
 from sklearn.svm import SVR
 import numpy as np
-n_samples, n_features = 10, 5
-np.random.seed(0)
-y = np.random.randn(n_samples)
-X = np.random.randn(n_samples, n_features)
+# n_samples, n_features = 10, 5
+# np.random.seed(0)
+X = [[0., 0.], [1., 1.]]
+y = [0, 1]
 clf = SVR(C=1.0, epsilon=0.2)
 print(clf.fit(X, y))
+print(clf.predict([[1., 1.]]))
