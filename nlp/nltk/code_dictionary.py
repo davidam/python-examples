db1d6e9ea075ebb87c2c0109671f4965d8a93eec
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-09-30 07:17:04 +0200

    nlp/nltk/code_dictionary.py: creation

--- nlp/nltk/code_dictionary.py
+++ nlp/nltk/code_dictionary.py
@@ -4,13 +4,14 @@ from collections import defaultdict
 counts = defaultdict(int)
 from nltk.corpus import brown
 for (word, tag) in brown.tagged_words(categories='news', tagset='universal'):
-     counts[tag] += 1
+    counts[tag] += 1
 
-print counts['NOUN']
-print sorted(counts)
+print(counts['NOUN'])
+
+print(sorted(counts))
+
 
 from operator import itemgetter
 sorted(counts.items(), key=itemgetter(1), reverse=True)
 [t for t, c in sorted(counts.items(), key=itemgetter(1), reverse=True)]
-
 
