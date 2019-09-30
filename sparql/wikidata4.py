9e3b456846b51b196d3b80c0be46c9c0496abd6d
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-09-30 17:24:36 +0200

    sparql/wikidata4.py: creation

---
+++ sparql/wikidata4.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with wikidata-example; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import requests
+
+url = 'https://query.wikidata.org/sparql'
+query = """
+SELECT
+  ?country ?countryLabel ?population ?area ?medianIncome
+WHERE {
+  ?country wdt:P463 wd:Q458.
+  ?country wdt:P1082 ?population.
+  ?country wdt:P2046 ?area.
+  ?country wdt:P3529 ?medianIncome.
+
+  SERVICE wikibase:label { bd:serviceParam wikibase:language "en". }
+}
+"""
+r = requests.get(url, params = {'format': 'json', 'query': query})
+data = r.json()
+print(data)
