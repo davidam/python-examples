4a8240236b08a4f9bef527b099472b4856a0d330
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-10-09 09:39:28 +0200

    rdflib/navigating.py, rdflib/n3.py, rdflib/literal.py: creation

---
+++ rdflib/literal.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with literal; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import datetime
+from rdflib import Literal, XSD
+lit2006 = Literal('2006-01-01',datatype=XSD.date)
+print(lit2006.toPython())
+print(lit2006 < Literal('2007-01-01',datatype=XSD.date))
+print(Literal(1) > Literal(2)) # by value
+print(Literal(1) > Literal(2.0)) # by value
+print(Literal('1') > Literal(1)) # by DT
+print(Literal('1') < Literal('1')) # by lexical form
+print(Literal('a', lang='en') > Literal('a', lang='fr')) # by lang-tag
---
+++ rdflib/navigating.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with navigating; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+from rdflib import ConjunctiveGraph, URIRef, RDFS, Literal
+from rdflib.namespace import SKOS
+from pprint import pprint
+g = ConjunctiveGraph()
+u = URIRef(u'http://example.com/foo')
+g.add([u, RDFS.label, Literal('foo')])
+g.add([u, RDFS.label, Literal('bar')])
+pprint(sorted(g.preferredLabel(u)))
+g.add([u, SKOS.prefLabel, Literal('bla')])
+pprint(g.preferredLabel(u))
+g.add([u, SKOS.prefLabel, Literal('blubb', lang='en')])
+sorted(g.preferredLabel(u))
+g.preferredLabel(u, lang='')
+pprint(g.preferredLabel(u, lang='en'))
