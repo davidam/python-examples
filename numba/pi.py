cbb306d1b668e725c03cb7356e9adcad4c27f8d8
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-11-12 17:21:22 +0100

    numba/pi.py: creation

--- numba/pi.py
+++ numba/pi.py
@@ -8,6 +8,12 @@ import numpy as np
 import argparse
 import time
 
+def calc_pi_single(n):
+    x = 2*np.random.ranf(n)-1
+    y = 2*np.random.ranf(n)-1
+    return 4*np.sum(x**2+y**2<1)/n
+
+
 run_parallel = numba.config.NUMBA_NUM_THREADS > 1
 
 @numba.njit(parallel=run_parallel)
@@ -29,5 +35,15 @@ def main():
     print("SELFTIMED ", selftimed)
     print("result: ", pi)
 
+    points_single = args.points
+    np.random.seed(0)
+
+    t1_single = time.time()
+    pi_single = calc_pi_single(points)
+    selftimed_single = time.time()-t1
+    print("SELFTIMED ", selftimed_single)
+    print("result: ", pi_single)
+
+    
 if __name__ == '__main__':
     main()
