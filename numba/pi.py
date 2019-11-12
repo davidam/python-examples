38ab4f65d202a73181f82ab0f850ae8a25bf516a
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2019-11-12 17:24:03 +0100

    numba/pi.py: bsd to gplv3

--- numba/pi.py
+++ numba/pi.py
@@ -1,7 +1,25 @@
-#
-# Copyright (c) 2017 Intel Corporation
-# SPDX-License-Identifier: BSD-2-Clause
-#
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with pi; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
 
 import numba
 import numpy as np
@@ -44,6 +62,6 @@ def main():
     print("SELFTIMED ", selftimed_single)
     print("result: ", pi_single)
 
-    
+
 if __name__ == '__main__':
     main()
