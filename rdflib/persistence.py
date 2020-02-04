db8621b4243471ebfa609de64cbaedfdb260e32f
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-02-04 15:23:05 +0100

    rdflib/persistence.py: creation

---
+++ rdflib/persistence.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with persistence; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+from rdflib import Namespace
+
+n = Namespace("http://example.org/")
+print(n)
+print(n.Person) # as attribute
+# = rdflib.term.URIRef(u'http://example.org/Person')
+
+print(n['first%20name'])
