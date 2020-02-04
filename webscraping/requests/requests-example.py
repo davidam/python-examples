f3c27598992d5fc3828703095e1d614b6a749410
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-02-04 10:35:24 +0100

    requests-example.py: add else

--- webscraping/requests/requests-example.py
+++ webscraping/requests/requests-example.py
@@ -7,3 +7,5 @@ if len(sys.argv) > 1:
     search = ' '.join(sys.argv[1:])
     res = requests.get('http://google.com/search?q=' + search )
     print(res.text)
+else:
+    print("Usted debe meter algún argumento")
