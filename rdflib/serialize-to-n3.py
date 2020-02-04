6af83f2bde1a29efdd95c23d52d2fa5c807d164a
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-02-04 15:24:18 +0100

    rdflib/serialize-to-n3.py: creation

---
+++ rdflib/serialize-to-n3.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2019  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with serialize-to-n3; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+
+from rdflib import Graph, URIRef, Literal, BNode
+from rdflib.namespace import FOAF, NamespaceManager
+
+person = URIRef('http://xmlns.com/foaf/0.1/Person')
+print(person.n3())
+
+g = Graph()
+g.bind("foaf", FOAF)
+
+print(person.n3(g.namespace_manager))
+
+l = Literal(2)
+l.n3()
+
+print(l.n3(g.namespace_manager))
