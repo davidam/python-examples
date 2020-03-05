e97d638866125b702f122bf1ab5f75687a1c057e
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-03-05 18:13:55 +0100

    network/get-example.py: creation

---
+++ network/get-example.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2020  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with python-examples; see the file LICENSE.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+
+import socket
+request = b"GET / HTTP/1.1\nHost: www.cnn.com\n\n"
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("cnn.com", 80))
+s.send(request)
+result = s.recv(10000)
+while (len(result) > 0):
+    print(result)
+    result = s.recv(10000)
