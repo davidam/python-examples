3278fc3ad9144b9f115ffb1c6dcdf9c1806cab1f
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-06-09 10:10:50 +0200

    basics/columns-in-csv.py: creation

--- basics/columns-in-csv.py
+++ basics/columns-in-csv.py
@@ -1,7 +1,6 @@
 import csv
 
 with open('csvfiles/all.csv', 'r') as csvfile:
-#    reader = csv.reader(csvfile, delimiter=',', quotechar='|')   
      first_line = csvfile.readline()
      print(first_line)
      ncol = first_line.count(',') + 1
