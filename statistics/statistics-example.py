17c87d7dfdebfe5c626864b629366884a160436c
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-08-20 02:13:02 +0200

    statistics/statistics-example.py: more statistical python functions

--- statistics/statistics-example.py
+++ statistics/statistics-example.py
@@ -11,8 +11,23 @@ print(y)
 z = statistics.mode(example_list)
 print(z)
 
+v = statistics.median_low(example_list)
+print(v)
+
+w = statistics.median_high(example_list)
+print(w)
+
+vv = statistics.median_grouped(example_list)
+print(vv)
+
 a = statistics.stdev(example_list)
 print(a)
 
 b = statistics.variance(example_list)
 print(b)
+
+d = statistics.harmonic_mean(example_list)
+print(d)
+
+e = statistics.pvariance(example_list)
+print(e)
