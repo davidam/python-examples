aed99fdbd2d93a9442e03b0448d70182c7822e23
David Arroyo Menéndez <d.arroyome@alumnos.urjc.es>
2020-09-12 22:53:20 +0200

    perceval/perceval_launchpad.py: print names and hide for

--- perceval/perceval_launchpad.py
+++ perceval/perceval_launchpad.py
@@ -2,6 +2,7 @@
 
 from datetime import datetime, timedelta
 from perceval.backends.core.launchpad import Launchpad
+from pprint import pprint
 
 # name 
 name = "ubuntu"
@@ -12,7 +13,9 @@ repo = Launchpad(name)
 
 
 # fetch all reviews as an iterator, and iterate it printing each review id
-for review in repo.fetch(from_date=from_date):
-    print(review)
+# for review in repo.fetch(from_date=from_date):
+#     pprint(review)
 
+for review in repo.fetch(from_date=from_date):
+    print(review['data']['activity_data'][0]['person_data']['display_name'])
     
