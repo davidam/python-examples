c06e105f8842ed64121b0c458a313a4dd11edbb0
David Arroyo Menéndez <davidam@gmail.com>
2020-11-18 20:24:53 +0100

    TCPServer.py: to python3

--- network/TCPServer.py
+++ network/TCPServer.py
@@ -12,8 +12,8 @@ class MyTCPHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         # self.request is the TCP socket connected to the client
         self.data = self.request.recv(1024).strip()
-        print "{} wrote:".format(self.client_address[0])
-        print self.data
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
         # just send back the same data, but upper-cased
         self.request.sendall(self.data.upper())
 
