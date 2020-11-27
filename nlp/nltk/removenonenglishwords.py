f7fcd4d4370c43f17e07ea952af43c3af5b3e67e
David Arroyo Menéndez <davidam@gmail.com>
2020-11-27 17:10:57 +0100

    nlp/nltk/removenonenglishwords.py: creation

---
+++ nlp/nltk/removenonenglishwords.py
@@ -0,0 +1,31 @@
+#  Copyright (C) 2020 David Arroyo Menéndez
+
+#  Author: David Arroyo Menéndez <davidam@gmail.com> 
+#  Maintainer: David Arroyo Menéndez <davidam@gmail.com> 
+#  This file is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3, or (at your option)
+#  any later version.
+# 
+#  This file is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+# 
+#  You should have received a copy of the GNU General Public License
+#  along with python-examples; see the file LICENSE.  If not, write to
+#  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, 
+#  Boston, MA 02110-1301 USA,
+
+
+import nltk
+words = set(nltk.corpus.words.words())
+
+sent = "Io andiamo to the beach with my amico."
+l = []
+
+for w in nltk.wordpunct_tokenize(sent):
+    if w.lower() in words or not w.isalpha():
+        l.append(w)
+        
+print(l)
