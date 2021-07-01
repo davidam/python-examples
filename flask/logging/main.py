9b7e396facbc7f8037ed0f3b16e1a7263026eeca
David Arroyo Menéndez <davidam@gmail.com>
2021-07-01 15:30:48 +0200

    flask/logging: creation

---
+++ flask/logging/main.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
+
+app = Flask(__name__)
+@app.route("/")
+def home():
+    app.logger.info('failed to log in')
+    return "<h1>Nginx & Gunicorn</h1>"
