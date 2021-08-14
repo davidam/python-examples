730001e666a23d48ee616d2e455c9118aa4c77a3
David Arroyo Menéndez <davidam@gmail.com>
2021-08-14 09:30:49 +0200

    flask/basics/templates/uploadfile.html, flask/basics/uploadfile.py: creation

---
+++ flask/basics/uploadfile.py
@@ -0,0 +1,32 @@
+
+
+# Importamos todo lo necesario
+import os
+from flask import Flask, render_template, request
+#from werkzeug import secure_filename
+
+# instancia del objeto Flask
+app = Flask(__name__)
+# Carpeta de subida
+app.config['UPLOAD_FOLDER'] = './pdffiles'
+
+@app.route("/")
+def upload_file():
+ # renderiamos la plantilla "formulario.html"
+ return render_template('uploadfile.html')
+
+@app.route("/upload", methods=['POST'])
+def uploader():
+ if request.method == 'POST':
+  # obtenemos el archivo del input "archivo"
+  f = request.files['archivo']
+#  filename = secure_filename(f.filename)
+  filename = f.filename
+  # Guardamos el archivo en el directorio "Archivos PDF"
+  f.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+  # Retornamos una respuesta satisfactoria
+  return "<h1>Archivo subido exitosamente</h1>"
+
+if __name__ == '__main__':
+ # Iniciamos la aplicación
+ app.run(debug=True)
