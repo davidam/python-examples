39d4f43e2eeb333e92baf61790773eb4ef256fa4
David Arroyo Menéndez <davidam@gmail.com>
2021-10-30 10:36:24 +0200

    basics/zip.py: the project is python-examples

--- basics/zip.py
+++ basics/zip.py
@@ -17,12 +17,14 @@
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with GNU Emacs; see the file COPYING.  If not, write to
+# along with python-examples; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
 v = [1, 2, 3]
-w = [1, 2, 3]
+w = [10, 20, 30]
 z = []
+
+print(zip(v,w))
 for v_i, w_i in zip(v,w):
     z = z + [v_i + w_i]
 print(z)
