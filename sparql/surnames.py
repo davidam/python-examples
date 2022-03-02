bb057386fc161760dabd5ea1e36f4681375c803a
David Arroyo Menéndez <davidam@gmail.com>
2022-03-02 17:12:15 +0100

    sparql/surnames.py: dumping results to surnames.csv

--- sparql/surnames.py
+++ sparql/surnames.py
@@ -17,29 +17,52 @@
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with wikidata-example; see the file LICENSE.  If not, write to
+# along with python-examples; see the file COPYING.  If not, write to
 # the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 # Boston, MA 02110-1301 USA,
 
-import requests
+from SPARQLWrapper import SPARQLWrapper, JSON
 
-url = 'https://query.wikidata.org/sparql'
+endpoint_url = "https://query.wikidata.org/sparql"
+
 query = """
+PREFIX bd: <http://www.bigdata.com/rdf#>
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX wikibase: <http://wikiba.se/ontology#>
+#added before 2016-10
+
+#defaultView:BubbleChart
 SELECT ?surname ?surnameLabel ?count
 WHERE
 {
   {
-    SELECT ?surname (COUNT(?human) AS ?count) WHERE {
-    # ?human wdt:P31 wd:Q5.
-      ?human wdt:P734 ?surname.
+    SELECT ?surname (COUNT(?person) AS ?count) WHERE {
+      ?person (wdt:P31/wdt:P279*) wd:Q95074.
+      ?person wdt:P734 ?surname.
     }
-    GROUP BY ?surname ORDER BY DESC(?count) LIMIT 100
+    GROUP BY ?surname
   }
   SERVICE wikibase:label { bd:serviceParam wikibase:language "[AUTO_LANGUAGE],en". }
 }
 ORDER BY DESC(?count)
-# limit to 10 results so we don't timeout
-LIMIT 10"""
-r = requests.get(url, params = {'format': 'json', 'query': query})
-data = r.json()
-print(data)
+LIMIT 100"""
+
+
+def get_results(endpoint_url, query):
+    sparql = SPARQLWrapper(endpoint_url)
+    sparql.setQuery(query)
+    sparql.setReturnFormat(JSON)
+    return sparql.query().convert()
+
+
+results = get_results(endpoint_url, query)
+
+print("Dumping results to surnames.csv")
+
+fo = open("surnames.csv", "w")
+for result in results["results"]["bindings"]:
+    fo.write(result['surnameLabel']['value'] + "," + result['count']['value'] +  "\n");
+
+# Cerramos el archivo fichero.txt
+fo.close()
