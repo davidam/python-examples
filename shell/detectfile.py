1f6b20de1ee1066019acc2c9a706eeff773d2abe
David Arroyo Menendez <darroyome@alumnos.urjc.es>
2022-09-29 19:37:22 -0300

    shell/detectfile.py: creation

---
+++ shell/detectfile.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# Copyright (C) 2018  David Arroyo Menéndez
+
+# Author: David Arroyo Menéndez <davidam@gnu.org>
+# Maintainer: David Arroyo Menéndez <davidam@gnu.org>
+
+# This file is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3, or (at your option)
+# any later version.
+
+# This file is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs; see the file COPYING.  If not, write to
+# the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+# Boston, MA 02110-1301 USA,
+#!/usr/bin/env python
+
+import os
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", help="display the gender")
+args = parser.parse_args()
+
+cmd = os.system('file --mime-type {0}'.format(args.filename))
+
+#result = subprocess.check_output(cmd)
+#pprint(result)
