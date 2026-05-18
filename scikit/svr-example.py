e3c1c669b60f2be2d0568499f8d70d06f0d1f66c
David Arroyo Menéndez <davidam@gmail.com>
2026-05-18 14:32:25 +0200

    scikit/svr-example.py: fix numpy array creation

--- scikit/svr-example.py
+++ scikit/svr-example.py
@@ -5,8 +5,10 @@ from sklearn.svm import SVR
 import numpy as np
 # n_samples, n_features = 10, 5
 # np.random.seed(0)
-X = [[0., 0.], [1., 1.]]
-y = [0, 1]
+X = np.array([[0., 0.], [1., 1.]])
+y = np.array([0, 1])
 clf = SVR(C=1.0, epsilon=0.2)
 print(clf.fit(X, y))
 print(clf.predict([[1., 1.]]))
+print(type(clf))
+print(type(clf.predict([[1., 1.]])))
