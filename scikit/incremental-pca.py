af0e5bfc97dfc09d9c4037efc4beb43ec5c3912a
David Arroyo Menéndez <davidam@gmail.com>
2026-05-18 14:22:58 +0200

    scikit/incremental-pca.py: n_components is the minimium of features and examples

--- scikit/incremental-pca.py
+++ scikit/incremental-pca.py
@@ -4,7 +4,8 @@
 import numpy as np
 from sklearn.decomposition import IncrementalPCA
 X = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
-print(X)
-ipca = IncrementalPCA(n_components=2, batch_size=3)
+num_components = min(X.shape)
+print("X is " % X)
+ipca = IncrementalPCA(n_components=num_components, batch_size=3)
 ipca.fit(X)
 print(ipca.transform(X))
