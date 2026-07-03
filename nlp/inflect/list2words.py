76e76062953c0c82cb13358421e81327e79aae53
David Arroyo Menéndez <davidam@gmail.com>
2026-07-03 15:06:16 +0200

    nlp/inflect/list2words.py: creation

---
+++ nlp/inflect/list2words.py
@@ -0,0 +1,16 @@
+# Program to pluralize a given
+# word in Python
+
+# Import the libraries
+import inflect
+
+# Declare method of inflect module
+p = inflect.engine()
+
+print(p.join(("apple", "banana", "carrot")))
+print(p.join(("apple", "banana")))
+print(p.join(("apple", "banana", "carrot"), final_sep=""))
+print(p.join(('apples', 'bananas', 'carrots'), conj='and even'))
+print(p.join(('apple', 'banana', 'carrot'), sep='/', sep_spaced=False, conj='', conj_spaced=False))
+
+
