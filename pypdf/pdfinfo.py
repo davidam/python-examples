55cd35211e91c2bd884636d8da1cba7cb0622d50
David Arroyo Menéndez <davidam@gmail.com>
2026-07-18 14:27:41 +0200

    pypdf/pdfinfo.py: updated source

--- pypdf/pdfinfo.py
+++ pypdf/pdfinfo.py
@@ -1,10 +1,14 @@
-from PyPDF2 import PdfFileReader
-p = r'one.pdf'
-pdf = PdfFileReader(file(p, 'rb'))
-print(pdf.documentInfo)
-print(pdf.getNumPages())
-info = pdf.getDocumentInfo()
-print(info.author)
-print(info.creator)
-print(info.producer)
-print(info.title)
+from pypdf import PdfReader
+
+reader = PdfReader("one.pdf")
+
+meta = reader.metadata
+
+# All the following could be None!
+print(meta.title)
+print(meta.author)
+print(meta.subject)
+print(meta.creator)
+print(meta.producer)
+print(meta.creation_date)
+print(meta.modification_date)
