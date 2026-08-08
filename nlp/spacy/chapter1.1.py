e18eb68e5763275f205089192a9a492b1fc28f35
David Arroyo Menéndez <davidam@gmail.com>
2026-08-08 14:06:32 +0200

    nlp/spacy: solving exercises from https://course.spacy.io/en/chapter1

---
+++ nlp/spacy/chapter1.1.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#  Copyright (C) 2026 David Arroyo Menéndez
+
+#  Author: David Arroyo Menéndez <davidam@gmail.com>
+#  Maintainer: David Arroyo Menéndez <davidam@gmail.com>
+#  This file is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3, or (at your option)
+#  any later version.
+#
+#  This file is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with python-examples; see the file LICENSE.txt.  If not, write to
+#  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#  Boston, MA 02110-1301 USA,
+
+#  Solved exercises from https://course.spacy.io/en/chapter1
+
+import spacy
+nlp = spacy.load("en_core_web_sm")
+doc = nlp("Hello world!")
+
+# Iterate over tokens in a Doc
+for token in doc:
+    print(token.text)
---
+++ nlp/spacy/chapter1.4.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#  Copyright (C) 2026 David Arroyo Menéndez
+
+#  Author: David Arroyo Menéndez <davidam@gmail.com>
+#  Maintainer: David Arroyo Menéndez <davidam@gmail.com>
+#  This file is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3, or (at your option)
+#  any later version.
+#
+#  This file is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with python-examples; see the file LICENSE.txt.  If not, write to
+#  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#  Boston, MA 02110-1301 USA,
+
+#  Solved exercises from https://course.spacy.io/en/chapter1
+
+
+import spacy
+nlp = spacy.load("en_core_web_sm")
+
+doc = nlp("It costs $5.")
+
+print("Index:   ", [token.i for token in doc])
+print("Text:    ", [token.text for token in doc])
+
+print("is_alpha:", [token.is_alpha for token in doc])
+print("is_punct:", [token.is_punct for token in doc])
+print("like_num:", [token.like_num for token in doc])
