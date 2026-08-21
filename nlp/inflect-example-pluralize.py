b8dbaf051607bfaaf539003ccebd30320a770fe2
David Arroyo Menéndez <davidam@gmail.com>
2026-08-21 14:06:29 +0200

    deleting inflect-example-pluralize.py

--- nlp/inflect-example-pluralize.py
+++
@@ -1,8 +0,0 @@
-import inflect
-engine = inflect.engine()
-plural = engine.plural("man")
-print(plural)
-plural = engine.plural("child")
-print(plural)
-plural = engine.plural("house")
-print(plural)
