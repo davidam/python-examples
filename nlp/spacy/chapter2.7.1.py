8c60754bd9aef740ee76f4b19f0133e3a79fc017
David Arroyo Menéndez <davidam@gmail.com>
2026-08-29 14:37:08 +0200

    nlp/spacy/chapter2.7.1.py: creation

---
+++ nlp/spacy/chapter2.7.1.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#  Copyright (C) 2026 David Arroyo Menéndez
+
+#  Author: David Arroyo Menéndez <davidam@gmail.com>
+#  Maintainer: David Arroyo Menéndez <davidam@gmail.com>
+#  This file is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3, or (at your option)
+#  any later version.
+#
+#  This file is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with python-examples; see the file LICENSE.txt.  If not, write to
+#  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#  Boston, MA 02110-1301 USA,
+
+#  Solved exercises from https://course.spacy.io/en/chapter2
+
+# Chapter 2: Large-scale data analysis with spaCy
+
+# 2.7 Data structures best practices
+
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp("Berlin looks like a nice city")
+
+# Iterate over the tokens
+for token in doc:
+    # Check if the current token is a proper noun
+    if token.pos_ == "PROPN":
+        # Check if the next token is a verb
+        if doc[token.i + 1].pos_ == "VERB":
+            print("Found proper noun before a verb:", token.text)
