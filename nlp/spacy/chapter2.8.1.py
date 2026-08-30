699891cf01426e3b3e38a6504b94788937fd5044
David Arroyo Menéndez <davidam@gmail.com>
2026-08-30 14:46:09 +0200

    nlp/spacy/chapter2.8.1.py: creation

---
+++ nlp/spacy/chapter2.8.1.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#  Copyright (C) 2026 David Arroyo Menéndez
+
+#  Author: David Arroyo Menéndez <davidam@gmail.com>
+#  Maintainer: David Arroyo Menéndez <davidam@gmail.com>
+#  This file is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3, or (at your option)
+#  any later version.
+#
+#  This file is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with python-examples; see the file LICENSE.txt.  If not, write to
+#  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#  Boston, MA 02110-1301 USA,
+
+#  Solved exercises from https://course.spacy.io/en/chapter2
+
+# Chapter 2: Large-scale data analysis with spaCy
+
+# 2.8 Word vectors and semantic similarity
+
+import spacy
+
+# Load a larger pipeline with vectors
+nlp = spacy.load("en_core_web_sm")
+
+# Compare two documents
+doc1 = nlp("I like fast food")
+doc2 = nlp("I like pizza")
+print(doc1.similarity(doc2))
+
+# Compare two tokens
+doc = nlp("I like pizza and pasta")
+token1 = doc[2]
+token2 = doc[4]
+print(token1.similarity(token2))
+
+# Compare a document with a token
+doc = nlp("I like pizza")
+token = nlp("soap")[0]
+print(doc.similarity(token))
+
+# Compare a span with a document
+span = nlp("I like pizza and pasta")[2:5]
+doc = nlp("McDonalds sells burgers")
+print(span.similarity(doc))
+
+# Similarity depends on the application context
+doc1 = nlp("I like cats")
+doc2 = nlp("I hate cats")
+
+print(doc1.similarity(doc2))
