fe4ea53292267c91b17c4d3fd957a71c3bf513e4
David Arroyo Menéndez <davidam@gmail.com>
2026-09-01 14:40:22 +0200

    nlp/spacy/chapter2.12.1.py: creation

---
+++ nlp/spacy/chapter2.12.1.py
@@ -0,0 +1,48 @@
+#  Copyright (C) 2026 David Arroyo Menéndez
+
+#  Author: David Arroyo Menéndez <davidam@gmail.com>
+#  Maintainer: David Arroyo Menéndez <davidam@gmail.com>
+#  This file is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3, or (at your option)
+#  any later version.
+#
+#  This file is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with python-examples; see the file LICENSE.txt.  If not, write to
+#  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#  Boston, MA 02110-1301 USA,
+
+
+import spacy
+from spacy.matcher import Matcher
+
+nlp = spacy.load("en_core_web_sm")
+doc = nlp(
+    "Twitch Prime, the perks program for Amazon Prime members offering free "
+    "loot, games and other benefits, is ditching one of its best features: "
+    "ad-free viewing. According to an email sent out to Amazon Prime members "
+    "today, ad-free viewing will no longer be included as a part of Twitch "
+    "Prime for new members, beginning on September 14. However, members with "
+    "existing annual subscriptions will be able to continue to enjoy ad-free "
+    "viewing until their subscription comes up for renewal. Those with "
+    "monthly subscriptions will have access to ad-free viewing until October 15.")
+
+# Create the match patterns
+pattern1 = [{"LOWER": "Amazon"}, {"IS_TITLE": True, "POS": "PROPN"}]
+pattern2 = [{"LOWER": "ad"}, {"TEXT":"-"}, {"LOWER": "free"}, {"POS": "NOUN"}]
+
+# Initialize the Matcher and add the patterns
+matcher = Matcher(nlp.vocab)
+matcher.add("PATTERN1", [pattern1])
+matcher.add("PATTERN2", [pattern2])
+matches = matcher(doc)
+
+# Iterate over the matches
+for match_id, start, end in matches:
+    # Print pattern string name and text of matched span
+    print(doc.vocab.strings[match_id], doc[start:end].text)
