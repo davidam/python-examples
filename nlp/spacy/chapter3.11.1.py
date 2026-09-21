3f9f02152a71c8bd9f9a5e637c893236fe673a21
David Arroyo Menéndez <davidam@gmail.com>
2026-09-21 15:01:01 +0200

    nlp/spacy/chapter3.11.1.py: creation

---
+++ nlp/spacy/chapter3.11.1.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#  Copyright (C) 2026 David Arroyo Menéndez
+
+#  Author: David Arroyo Menéndez <davidam@gmail.com>
+#  Maintainer: David Arroyo Menéndez <davidam@gmail.com>
+#  This file is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3, or (at your option)
+#  any later version.
+#
+#  This file is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with python-examples; see the file LICENSE.txt.  If not, write to
+#  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#  Boston, MA 02110-1301 USA,
+
+# Solved exercises from https://course.spacy.io/en/chapter3
+
+import spacy
+from spacy.tokens import Span
+
+nlp = spacy.load("en_core_web_sm")
+
+
+def get_wikipedia_url(span):
+    # Get a Wikipedia URL if the span has one of the labels
+    if span.label_ in ("PERSON", "ORG", "GPE", "LOCATION"):
+        entity_text = span.text.replace(" ", "_")
+        return "https://en.wikipedia.org/w/index.php?search=" + entity_text
+
+
+# Set the Span extension wikipedia_url using the getter get_wikipedia_url
+Span.set_extension("wikipedia_url", getter=get_wikipedia_url)
+
+doc = nlp(
+    "In over fifty years from his very first recordings right through to his "
+    "last album, David Bowie was at the vanguard of contemporary culture."
+)
+for ent in doc.ents:
+    # Print the text and Wikipedia URL of the entity
+    print(ent.text, ent._.wikipedia_url)
