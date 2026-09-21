2332536ba39d5dd7d8a0677ab5db27ae3283ca31
David Arroyo Menéndez <davidam@gmail.com>
2026-09-21 14:59:17 +0200

    nlp/spacy/chapter3.9.2.py: creation

---
+++ nlp/spacy/chapter3.9.2.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#  Copyright (C) 2026 David Arroyo Menéndez
+
+#  Author: David Arroyo Menéndez <davidam@gmail.com>
+#  Maintainer: David Arroyo Menéndez <davidam@gmail.com>
+#  This file is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3, or (at your option)
+#  any later version.
+#
+#  This file is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with python-examples; see the file LICENSE.txt.  If not, write to
+#  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#  Boston, MA 02110-1301 USA,
+
+# Solved exercises from https://course.spacy.io/en/chapter3
+
+import spacy
+from spacy.tokens import Token
+
+nlp = spacy.blank("en")
+
+# Define the getter function that takes a token and returns its reversed text
+def get_reversed(token):
+    return token.text[::-1]
+
+
+# Register the Token property extension "reversed" with the getter get_reversed
+Token.set_extension("reversed", getter=get_reversed)
+
+# Process the text and print the reversed attribute for each token
+doc = nlp("All generalizations are false, including this one.")
+for token in doc:
+    print("reversed:", token._.reversed)
