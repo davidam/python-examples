ae3b20d8f9dee2cbe2794e354d60c1622203ad46
David Arroyo Menéndez <davidam@gmail.com>
2026-09-21 14:59:00 +0200

    nlp/spacy/chapter3.9.1.py: creation

---
+++ nlp/spacy/chapter3.9.1.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#  Copyright (C) 2026 David Arroyo Menéndez
+
+#  Author: David Arroyo Menéndez <davidam@gmail.com>
+#  Maintainer: David Arroyo Menéndez <davidam@gmail.com>
+#  This file is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 3, or (at your option)
+#  any later version.
+#
+#  This file is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with python-examples; see the file LICENSE.txt.  If not, write to
+#  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+#  Boston, MA 02110-1301 USA,
+
+# Solved exercises from https://course.spacy.io/en/chapter3
+
+import spacy
+from spacy.tokens import Token
+
+nlp = spacy.blank("en")
+
+# Register the Token extension attribute "is_country" with the default value False
+Token.set_extension("is_country", default=False)
+
+# Process the text and set the is_country attribute to True for the token "Spain"
+doc = nlp("I live in Spain.")
+doc[3]._.is_country = True
+
+# Print the token text and the is_country attribute for all tokens
+print([(token.text, token._.is_country) for token in doc])
