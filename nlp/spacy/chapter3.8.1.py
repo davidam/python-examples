2641852c4c66337eead35eefaabe44fe4abf3f05
David Arroyo Menéndez <davidam@gmail.com>
2026-09-21 14:58:08 +0200

    nlp/spacy/chapter3.8.1.py: adding documentation

--- nlp/spacy/chapter3.8.1.py
+++ nlp/spacy/chapter3.8.1.py
@@ -19,6 +19,8 @@
 #  the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
 #  Boston, MA 02110-1301 USA,
 
+# Solved exercises from https://course.spacy.io/en/chapter3
+
 import spacy
 from spacy.tokens import Token
 
