fd0cbc1eae0703198156795d08ca3440977da599
David Arroyo Menéndez <davidam@gmail.com>
2026-09-23 14:49:48 +0200

    nlp/spacy/chapter4.1.1.py: creating model and saving to disk

--- nlp/spacy/chapter4.1.1.py
+++ nlp/spacy/chapter4.1.1.py
@@ -1,5 +1,8 @@
+import random
 import spacy
 from spacy.tokens import Span
+from spacy.tokens import DocBin
+
 
 nlp = spacy.blank("en")
 
@@ -11,4 +14,17 @@ doc2 = nlp("I need a new phone! Any tips?")
 
 docs = [doc1, doc2]
 
-print(docs)
+#print(docs)
+
+random.shuffle(docs)
+train_docs = docs[:len(docs) // 2]
+print(train_docs)
+dev_docs = docs[len(docs) // 2:]
+print(dev_docs)
+
+# Create and save a collection of training docs
+train_docbin = DocBin(docs=train_docs)
+train_docbin.to_disk("./train.spacy")
+# Create and save a collection of evaluation docs
+dev_docbin = DocBin(docs=dev_docs)
+dev_docbin.to_disk("./dev.spacy")
